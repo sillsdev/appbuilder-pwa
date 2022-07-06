a331601918c91f06652d2d4b3e3aff7cacd8ceda
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2022-07-06 13:58:33 -0400

    Add watch functionality to build scripts

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,19 +1,38 @@
 import { convertConfig } from './convertConfig';
 import { convertMedia } from './convertMedia';
+import { convertBooks } from './convertBooks';
+import { convertAbout } from './convertAbout';
+import { watch } from 'chokidar';
+
+// Possible arguments:
+// --data-dir=<path> (override data path)
+// --examples (convert examples; incompatible with --data-dir)
+// --watch (watch for changes)
+// --watch-timeout=<ms> (time between changes)
+
+const suppliedDataDir = process.argv.find((arg) => arg.includes('--data-dir'));
+const dataDir = suppliedDataDir
+    ? suppliedDataDir.split('=')[1]
+    : process.argv.includes('--examples')
+    ? 'example_data'
+    : 'data';
+
+const watchTimeoutArg = process.argv.find((arg) => arg.includes('--watch-timeout'));
+const watchTimeout = watchTimeoutArg ? parseInt(watchTimeoutArg.split('=')[1]) : 100;
 
 // Not incremental
-const steps = [convertConfig, convertMedia].map((f) =>
-    f.bind(null, process.argv.includes('--examples') ? 'example_data' : 'data')
-);
+const steps = [convertConfig, convertMedia, convertBooks, convertAbout];
 
 const oldConsoleLog = console.log;
 const oldConsoleError = console.error;
 let currentStep = 0;
+let currentWatchOutput = '';
 let currentOutput = '';
 let currentErrors = '';
 
 function print() {
     console.clear();
+    oldConsoleLog(currentWatchOutput);
     oldConsoleLog(currentOutput);
     oldConsoleError(currentErrors);
     oldConsoleLog(`${steps[currentStep].name} (${currentStep + 1}/${steps.length})...`);
@@ -29,15 +48,16 @@ console.error = (...args) => {
     print();
 };
 
-function convert() {
+async function convert() {
     currentErrors = '';
     currentStep = 0;
     for (const step of steps) {
         currentOutput = '';
         try {
-            step();
+            // step may be async, in which case it should be awaited
+            await step(dataDir);
         } catch (e) {
-            console.log('Error: ' + e);
+            console.log(e);
             return;
         }
         currentStep++;
@@ -46,8 +66,18 @@ function convert() {
 
 // TODO: watch mode/examples
 if (process.argv.includes('--watch')) {
-    console.log('Watching for changes...');
-    setInterval(convert, 1000);
-} else {
-    convert();
+    currentWatchOutput += 'Watching for changes...\n';
+    // watch for changes with chokidar
+    let timer: NodeJS.Timeout;
+
+    watch(dataDir).on('all', async (event, path) => {
+        currentWatchOutput += `${path} changed\n`;
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(async () => {
+            await convert();
+            currentWatchOutput = 'Watching for changes...\n';
+        }, watchTimeout);
+        print();
+    });
 }
+convert();
