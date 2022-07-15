3cc466da878ffdd0ba19bebe15e128e0c5dcde10
FyreByrd <aejones4gm@gmail.com>
2022-07-15 10:50:19 -0500

    update icons

---
+++ src/lib/icons/index.js
@@ -0,0 +1,36 @@
+//exporting object with all the icons to make icon imports "cleaner" elsewhere
+import AboutIcon from './AboutIcon.svelte';
+import BibleIcon from './BibleIcon.svelte';
+import BookmarkIcon from './BookmarkIcon.svelte';
+import DropdownIcon from './DropdownIcon.svelte';
+import HamburgerIcon from './HamburgerIcon.svelte';
+import HighlightIcon from './HighlightIcon.svelte';
+import HistoryIcon from './HistoryIcon.svelte';
+import MuteIcon from './MuteIcon.svelte';
+import NoteIcon from './NoteIcon.svelte';
+import SearchIcon from './SearchIcon.svelte';
+import SettingsIcon from './SettingsIcon.svelte';
+import ShareIcon from './ShareIcon.svelte';
+import SideBySideIcon from './SideBySideIcon.svelte';
+import SinglePaneIcon from './SinglePaneIcon.svelte';
+import TextAppearanceIcon from './TextAppearanceIcon.svelte';
+import VerseByVerseIcon from './VerseByVerseIcon.svelte';
+
+export {
+    AboutIcon,
+    BibleIcon,
+    BookmarkIcon,
+    DropdownIcon,
+    HamburgerIcon,
+    HighlightIcon,
+    HistoryIcon,
+    MuteIcon,
+    NoteIcon,
+    SearchIcon,
+    SettingsIcon,
+    ShareIcon,
+    SideBySideIcon,
+    SinglePaneIcon,
+    TextAppearanceIcon,
+    VerseByVerseIcon
+};
