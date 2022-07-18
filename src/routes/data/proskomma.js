7b9ad0809759d874f144ed70b55eddcee43feeb3
FyreByrd <aejones4gm@gmail.com>
2022-07-18 12:02:39 -0500

    Proskomma endpoint

---
+++ src/routes/data/proskomma.js
@@ -0,0 +1,44 @@
+import { Proskomma } from 'proskomma';
+import { thaw } from 'proskomma-freeze';
+import { collections } from '$lib/data/book-collections';
+
+const pk = (() => {
+    let _val = new Proskomma();
+
+    const memo = { initialized: null };
+    function memoInit() {
+        if (memo['initialized'] !== null) {
+            return memo['initialized'];
+        }
+
+        memo['initialized'] = init(); // memoize the promise for key
+        return memo['initialized'];
+    }
+
+    //thaws frozen archives
+    const init = async () => {
+        for (const c of collections) {
+            await thaw(_val, c);
+        }
+    };
+
+    const query = async (q, cb) => {
+        memoInit();
+        return _val.gqlQuery(q, cb);
+    };
+
+    const gqlQuery = async (q, cb) => {
+        query(q, cb);
+    };
+
+    return { query, gqlQuery };
+})();
+
+export async function post({ request }) {
+    const body = await request.json();
+    return {
+        body: {
+            text: await pk.query(body.query)
+        }
+    };
+}
