de648fc7e4b2f75fe2a99c36aac4f33af7a68fdb
FyreByrd <aejones4gm@gmail.com>
2022-07-18 12:22:05 -0500

    stores from sbj repo

--- src/lib/data/stores.ts
+++ src/lib/data/stores.ts
@@ -1,9 +1,11 @@
 import { derived, readable, writable } from 'svelte/store';
+import { groupStore, referenceStore } from './store-types';
 import globalConfigJson from '../../config';
 
 export const globalConfig = readable(globalConfigJson);
 export const activeBookName = writable('john');
 // Books are currently an array, which seems slower than necessary. Might be better as an object (for constant lookup time)
+// ^-- regarding this, proskomma-tools has a catalog function (I used in constants.js). could use to combine catalog into config...
 export const activeBook = derived(
     [activeBookName, globalConfig],
     ([$activeBookName, $globalConfig]) =>
@@ -12,3 +14,9 @@ export const activeBook = derived(
             (book) => book.name.toLowerCase() === $activeBookName.toLowerCase()
         )
 );
+
+export const refs = groupStore(referenceStore);
+
+export const scrolls = groupStore(writable, 'title');
+
+export const viewMode = writable('Side By Side');
