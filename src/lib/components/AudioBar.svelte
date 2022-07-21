bfeee61e124e4003aa49a774c87abf1d66f8cef2
FyreByrd <aejones4gm@gmail.com>
2022-07-21 12:13:19 -0500

    add audio icons to audio bar

--- src/lib/components/AudioBar.svelte
+++ src/lib/components/AudioBar.svelte
@@ -1,10 +1,16 @@
 <script>
-    import { AudioIcons } from '$lib/icons';
+    import { AudioIcon } from '$lib/icons';
     export let src = '';
     let audio;
 </script>
 
 <div class="w-full h-full">
     <audio bind:this={audio} {src} class="hidden w-0 h-0" />
-    
+    <div class="inline-flex">
+        <AudioIcon.Prev />
+        <AudioIcon.RW />
+        <AudioIcon.Play />
+        <AudioIcon.FF />
+        <AudioIcon.Skip />
+    </div>
 </div>
