1195e1c2f91abafaa706dd3e8216b6cbc651242f
FyreByrd <aejones4gm@gmail.com>
2022-07-21 12:11:27 -0500

    added new audio icons to AudioIcon

--- src/lib/icons/audio/index.js
+++ src/lib/icons/audio/index.js
@@ -1,9 +1,21 @@
+import FastForwardIcon from './FastForwardIcon.svelte';
 import MuteIcon from './MuteIcon.svelte';
+import PauseIcon from './PauseIcon.svelte';
+import PlayIcon from './PlayIcon.svelte';
+import RewindIcon from './RewindIcon.svelte';
+import SkipNextIcon from './SkipNextIcon.svelte';
+import SkipPreviousIcon from './SkipPreviousIcon.svelte';
 import VolumeIcon from './VolumeIcon.svelte';
 
 const AudioIcon = {
     Mute: MuteIcon,
-    Volume: VolumeIcon
+    Volume: VolumeIcon,
+    Pause: PauseIcon,
+    Play: PlayIcon,
+    FF: FastForwardIcon,
+    RW: RewindIcon,
+    Skip: SkipNextIcon,
+    Prev: SkipPreviousIcon
 };
 
 export { AudioIcon };
