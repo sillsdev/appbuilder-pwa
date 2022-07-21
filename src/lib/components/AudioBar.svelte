dc62d274be55798341d653779a36f756125fae1f
FyreByrd <aejones4gm@gmail.com>
2022-07-21 15:04:10 -0500

    audiobar layout

--- src/lib/components/AudioBar.svelte
+++ src/lib/components/AudioBar.svelte
@@ -1,16 +1,37 @@
 <script>
     import { AudioIcon } from '$lib/icons';
     export let src = '';
-    let audio;
+    /**@type{HTMLMediaElement}*/let audio;
 </script>
 
-<div class="w-full h-full">
-    <audio bind:this={audio} {src} class="hidden w-0 h-0" />
-    <div class="inline-flex">
-        <AudioIcon.Prev />
-        <AudioIcon.RW />
-        <AudioIcon.Play />
-        <AudioIcon.FF />
-        <AudioIcon.Skip />
+<div class="w-11/12 h-5/6 bg-base-100 mx-auto rounded-full flex items-center flex-col">
+    <audio bind:this={audio} {src} class="w-0 h-0" />
+    <div class="flex flex-col justify-center w-11/12 flex-grow">
+        {#if audio?.readyState}
+            <progress class="dy-progress w-full h-1 place-self-end m-2" 
+                value={audio.currentTime} 
+                max={audio.duration}>
+            </progress>
+        {:else}
+            <progress class="dy-progress w-full h-1 place-self-end mx-2 my-1" value=0 max=1>
+            </progress>
+        {/if}
+        <div class="dy-btn-group place-self-center">
+            <button class="dy-btn-sm dy-btn-ghost">
+                <AudioIcon.Prev />
+            </button>
+            <button class="dy-btn-sm dy-btn-ghost">
+                <AudioIcon.RW />
+            </button>
+            <button class="dy-btn-sm dy-btn-ghost">
+                <AudioIcon.Play />
+            </button>
+            <button class="dy-btn-sm dy-btn-ghost">
+                <AudioIcon.FF />
+            </button>
+            <button class="dy-btn-sm dy-btn-ghost">
+                <AudioIcon.Skip />
+            </button>
+        </div>
     </div>
 </div>
