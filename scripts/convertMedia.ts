86eaa897a7fb7a194ce45a0105f21b2d97905e8e
Chris Hubbard <chris@thehubbards.org>
2022-07-25 15:27:18 -0400

    In convertMedia.ts, images and illustrations are optional
    * Before, if there were no images, then the conversion would fail
      with a trackback in the output.
    * Add illustrations as optional media files to process

--- scripts/convertMedia.ts
+++ scripts/convertMedia.ts
@@ -1,13 +1,28 @@
-import { copyFileSync, cpSync } from 'fs';
+import { CopyOptions, cpSync } from 'fs';
 import path from 'path';
 
+function cpSyncOptional(source: string, destination: string, opts?: CopyOptions): void {
+    try {
+        cpSync(source, destination, opts);
+    } catch (e) {
+        // source doesn't exist, ok.
+    }
+}
+
 export function convertMedia(dataDir: string) {
-    // Copy the css stylesheet
-    copyFileSync(path.join(dataDir, 'sab-stylesheet.css'), path.join('static'));
+    // Copy the css stylesheets
+    cpSync(path.join(dataDir, 'styles'), path.join('static', 'styles'), { recursive: true });
 
     // Copy the font files
-    cpSync(path.join(dataDir, 'fonts'), path.join('static'), { recursive: true });
+    cpSync(path.join(dataDir, 'fonts'), path.join('static', 'fonts'), { recursive: true });
 
     // Copy the images
-    cpSync(path.join(dataDir, 'images'), path.join('static'), { recursive: true });
+    cpSyncOptional(path.join(dataDir, 'images'), path.join('static', 'images'), {
+        recursive: true
+    });
+
+    // Copy the illustrations
+    cpSyncOptional(path.join(dataDir, 'illustrations'), path.join('static', 'illustrations'), {
+        recursive: true
+    });
 }
