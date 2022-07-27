7e51337c538f142c5920a74ab86c16e7e43fd0c1
7dev7urandom <30197373+7dev7urandom@users.noreply.github.com>
2022-07-27 14:47:48 -0400

    Remove gitignored file
    Remove old files

--- src/lib/data/book-collections/index.js
+++
@@ -1,5 +0,0 @@
-import { eng_lsv_pent } from './eng_lsv_pent';
-import { eng_web_pent } from './eng_web_pent';
-import { rus_syn_pent } from './rus_syn_pent';
-
-export const collections = [eng_lsv_pent, eng_web_pent, rus_syn_pent];
