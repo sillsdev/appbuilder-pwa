1472262295f689c7670cdfba9b5d01fc940ed21e
FyreByrd <aejones4gm@gmail.com>
2022-08-01 11:37:03 -0500

    beginning of Proskomma dependent ScriptureView

--- src/lib/components/ScriptureView.svelte
+++ src/lib/components/ScriptureView.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { query } from '../scripts/query';
     import { onDestroy } from 'svelte';
     import { audioHighlight, refs, scrolls, playingAudio } from '$lib/data/stores';
     import { inview } from 'svelte-inview';
@@ -68,9 +69,60 @@
     const options = { threshold: 0.5 };
 
     onDestroy(unSub);
+
+    $: promise = query(`{
+        docSet(id:"${$refs['default'].docSet}") {
+            book: document(bookCode: "${$refs['default'].book}") {
+                main: mainSequence {
+                    blocks(withScriptureCV: "${$refs['default'].chapter}") {
+                        bs { payload }
+                        items { type subType payload }
+                    }
+                }
+            }
+        }
+    }`);
 </script>
 
 <article class="prose container mx-auto" bind:this={container}>
+    <div
+            id="title"
+            class="scroll-item"
+            use:inview={options}
+            on:change={(e) => handleChange(e, "title")}>
+            <h1>{$refs['default'].title}</h1>
+            <h2>Chapter {$refs['default'].chapter}</h2>
+        </div>
+    {#await promise}
+        <p>loading</p>
+    {:then res}
+        {#if Array.isArray(JSON.parse(res).data.docSet?.book?.main.blocks)}
+            {#each JSON.parse(res).data.docSet?.book?.main.blocks as block, i}
+                <div class="{i === 0?"m":"p"}">
+                    {#each block.items as item}
+                        {#if item.type === "scope" && item.subType === "start"}
+                            {#if item.payload.split("/")[0] === "verses"}
+                                <em id="{item.payload.split("/")[1]}"
+                                    class="scroll-item"
+                                    use:inview={options}
+                                    on:change={(e) => handleChange(e, item.payload.split("/")[1])}
+                                >{item.payload.split("/")[1]}</em><span>&nbsp;</span>
+                            {:else}
+                                <!---->
+                            {/if}
+                        {:else if item.type === "token"}
+                            {item.payload}
+                        {/if}
+                    {/each}
+                </div>
+            {/each}
+        {:else}
+            <p>waiting on Proskomma . . .</p>
+        {/if}
+    {:catch err}
+        <pre style="color: red">{err.message}</pre>
+    {/await}
+    <!--
     <h1
         id="title"
         class="text-center scroll-item"
@@ -107,6 +159,7 @@
             {/each}
         </p>
     {/each}
+    -->
 </article>
 
 <style>
