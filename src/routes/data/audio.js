238b1c738269a796b8907ee2ef2dca4f5d480aba
FyreByrd <aejones4gm@gmail.com>
2022-08-02 12:03:15 -0500

    add audio endpoint

---
+++ src/routes/data/audio.js
@@ -0,0 +1,46 @@
+import config from '../../config';
+import path from 'path';
+import { readFile } from 'fs/promises';
+
+export async function post({ request }) {
+    const body = await request.json();
+
+    const audio = config.bookCollections.find((c) => {
+        return body.collection.split('_')[0] === c.languageCode && 
+        body.collection.split('_')[1] === c.collectionAbbreviation;
+    })?.books?.find((b) => (b.id === body.book))?.audio?.find((a) => body.chapter === ''+a.num);
+
+    if(!audio) {
+        return {
+            body: {
+                error: `no audio found for ${body.book}:${body.chapter}`
+            }
+        }
+    }
+
+    console.log(JSON.stringify(audio));
+
+    const timingFile = await readFile(path.join('static', 'timings', audio.timingFile), 'utf8');
+    const lines = timingFile.split('separators ')[1].split('\n');
+    const timing = [];
+    timing.push({ 
+        time: parseFloat(lines[1].split('\t')[0]), 
+        tag: 'title'
+    });
+
+    for(let i = 1; i < lines.length; i++) {
+        if(lines[i]) {
+            timing.push({ 
+                time: parseFloat(lines[i].split('\t')[1]), 
+                tag: lines[i].split('\t')[2]
+            });
+        }
+    }
+
+    return {
+        body: {
+            source: audio.filename,
+            timing: timing
+        }
+    };
+}
