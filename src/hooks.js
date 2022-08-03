6f6699a8dab1de25d8eb86748e5268737511ccd7
FyreByrd <aejones4gm@gmail.com>
2022-08-03 15:52:22 -0500

    commented hooks.js

--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,4 @@
+//added handle function because of an SSR issue that was breaking the app
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
     const response = await resolve(event, {
