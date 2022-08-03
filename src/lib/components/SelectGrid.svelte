571196354c97557148cac0d99f759fe3e25d9ba1
FyreByrd <aejones4gm@gmail.com>
2022-08-03 14:06:11 -0500

    added comments to some UI components

--- src/lib/components/SelectGrid.svelte
+++ src/lib/components/SelectGrid.svelte
@@ -1,7 +1,11 @@
+<!--
+@component
+A component to display menu options in a grid.
+-->
 <script lang="ts">
     import { createEventDispatcher } from 'svelte';
     export let options = [''];
-    const cols = 5;
+    export let cols = 5;
     const dispatch = createEventDispatcher();
     $: rows = Math.ceil(options.length / cols);
 
@@ -12,12 +16,12 @@
     }
 </script>
 
-<table>
+<table class="bg-base-200">
     {#each Array(rows) as _, ri}
         <tr>
             {#each Array(cols) as _, ci}
                 {#if ri * cols + ci < options.length}
-                    <td
+                    <td class="border-base-200 bg-base-100"
                         ><span
                             on:click={() => handleClick(options[ri * cols + ci])}
                             class="dy-btn dy-btn-square dy-btn-ghost p-0 m-0"
@@ -37,7 +41,6 @@
         margin-right: auto;
         padding: 0px;
         border-collapse: unset;
-        background-color: lightgray;
     }
     tr {
         width: 100%;
@@ -50,9 +53,8 @@
         margin: 0px;
         padding: 0px;
         position: relative;
-        border: 1px solid lightgray;
+        border: 1px solid;
         border-radius: 5px;
-        background-color: white;
     }
     span {
         text-overflow: ''; /* Works on Firefox only */
