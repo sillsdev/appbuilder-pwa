d30d64b0b92431cb00968a3e2001099518e32645
FyreByrd <aejones4gm@gmail.com>
2022-08-05 12:02:59 -0500

    render individual phrases

--- src/lib/scripts/render.js
+++ src/lib/scripts/render.js
@@ -1,53 +1,73 @@
-
 const seps = '.?!:;,';
+const subc = 'abcdefghijklmnopqrstuvwxyz';
 
 export const renderBlocks = (root, blocks) => {
     if (!root || !blocks?.length) return;
     root.replaceChildren(); // clear current blocks from root
-    for (let i = 0; i < blocks.length; i ++)
-        root.append(renderBlock(blocks[i], i? 'p' : 'm'));
-}
+    for (let i = 0; i < blocks.length; i++) root.append(renderBlock(blocks[i], i ? 'p' : 'm'));
+};
 
 const renderBlock = (block, className) => {
     const div = document.createElement('div');
     div.className = className;
-    //console.log(JSON.stringify(block, null, 2));
     let i = 0;
     while (i < block.items.length) {
         const it = block.items[i];
-        if (it.subType === "start" && it.payload.split('/')[0] === 'verses') {
-            const o = renderVerse(block.items, block.items[i].payload.split('/')[1], i+1);
+        if (it.subType === 'start' && it.payload.split('/')[0] === 'verses') {
+            const o = renderVerse(block.items, block.items[i].payload.split('/')[1], i + 1);
             div.append(o.verse);
             i = o.i;
         }
         i++;
     }
     return div;
-}
+};
 
 const renderVerse = (items, verseNum, index) => {
     const verse = document.createElement('span');
     verse.append(verseNum + ' ');
+    verse.id = verseNum;
+    verse.className += 'scroll-item';
+    const phrases = [];
     let i = index;
     while (i < items.length) {
         const it = items[i];
-        if (it.type === "token") {
-            verse.append(items[i].payload);
-        }
-        else if (it.type === "scope" && it.subType === "end" && it.payload.split('/')[0] === 'verses') {
+        if (it.type === 'scope' && it.subType === 'end' && it.payload.split('/')[0] === 'verses') {
+            i++;
             break;
+        } else if (it.type === 'token') {
+            const o = renderPhrase(items, i, verseNum);
+            phrases.push(o.phrase);
+            i = o.i;
         }
-
         i++;
     }
-    
 
-    return { verse: verse, i: i }
-}
+    if (phrases.length > 1) {
+        for (let j = 0; j < phrases.length; j++) phrases[j].id += subc[j];
+    }
+    for (const phrase of phrases) verse.append(phrase);
 
-const renderPhrase = (items, index) => {
+    return { verse: verse, i: i };
+};
+
+const renderPhrase = (items, index, id) => {
     const phrase = document.createElement('span');
-    
+    phrase.id = id;
+    let s = '';
+    let i = index;
+    while (i < items.length) {
+        const it = items[i];
+        i++;
+        if (it.type === 'token') {
+            s += it.payload;
+            if (it.subType === 'punctuation' && seps.includes(it.payload)) {
+                s += ' ';
+                break;
+            }
+        }
+    }
+    phrase.append(s);
 
-    return { phrase: phrase, i: index }
-}
+    return { phrase: phrase, i: i };
+};
