2650d28aa4c2e3b209a0aa9b59ee0545f6efd7be
jakedcolbert <jake.colbert@gordon.edu>
2022-10-06 17:47:55 -0400

    Search, History, Bookmarks, Notes, and Highlights will display in drawer only if they are enabled in scripture app builder.

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -22,6 +22,12 @@ The sidebar/drawer.
         document.activeElement.blur();
     };
     beforeNavigate(closeDrawer);
+
+    let showSearch = $globalConfig.mainFeatures['search'];
+    let showHistory = $globalConfig.mainFeatures['history'];
+    let showBookmarks = $globalConfig.mainFeatures['annotation-bookmarks'];
+    let showNotes = $globalConfig.mainFeatures['annotation-notes'];
+    let showHighlights = $globalConfig.mainFeatures['annotation-highlights'];
 </script>
 
 <div class="dy-drawer dy-drawer-mobile">
@@ -35,15 +41,25 @@ The sidebar/drawer.
         <ul class="dy-menu p-1 overflow-y-auto w-3/4 sm:w-80 bg-base-100 text-base-content">
             <!-- Sidebar content here -->
             <li><a href="/"><BibleIcon /><span>{$globalConfig.name}</span></a></li>
-            <li><a href="/search"><SearchIcon />Search</a></li>
-            <div class="dy-divider m-1" />
-            <li><a href="/history"><HistoryIcon />History</a></li>
-            <li><a href="/bookmarks"><BookmarkIcon />Bookmarks</a></li>
-            <li><a href="/notes"><NoteIcon />Notes</a></li>
-            <li><a href="/highlights"><HighlightIcon />Highlights</a></li>
-            <div class="dy-divider m-1" />
+            {#if showSearch}
+              <li><a href="/search"><SearchIcon />Search</a></li>
+              <div class="dy-divider m-1" />
+            {/if}      
+            {#if showHistory}  
+              <li><a href="/history"><HistoryIcon />History</a></li>
+            {/if}    
+            {#if showBookmarks}
+              <li><a href="/bookmarks"><BookmarkIcon />Bookmarks</a></li>
+            {/if}    
+            {#if showNotes}
+              <li><a href="/notes"><NoteIcon />Notes</a></li>
+            {/if}    
+            {#if showHighlights}
+              <li><a href="/highlights"><HighlightIcon />Highlights</a></li>  
+              <div class="dy-divider m-1" />
+            {/if}    
             <li><a href="/share"><ShareIcon />Share App</a></li>
-            <div class="dy-divider m-1" />
+            <div class="dy-divider m-1" /> 
             <li><a href="/settings"><SettingsIcon />Settings</a></li>
             <!-- svelte-ignore a11y-missing-attribute -->
             <li><a on:click={closeDrawer}><TextAppearanceIcon />Text Appearance</a></li>
