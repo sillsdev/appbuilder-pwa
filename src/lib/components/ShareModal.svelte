97ca6e7c6273928d38418f8e5aec480bcafba3ea
jakedcolbert <jake.colbert@gordon.edu>
2022-10-07 12:14:27 -0400

    Progress so far

---
+++ src/lib/components/ShareModal.svelte
@@ -0,0 +1,24 @@
+<!--
+  @component
+  Share App pop up window.
+-->
+<script>
+    import { BibleIcon } from '$lib/icons';
+    import { globalConfig } from '$lib/data/stores';
+</script>
+
+<!-- The modal -->
+<input type="checkbox" id="share" class="modal-toggle" />
+<div class="modal">
+    <div class="modal-box relative">
+        <label for="share" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <h3 class="text-lg font-bold">Congratulations random Internet user!</h3>
+        <p class="py-4">
+            You've been selected for a chance to get one year of subscription to use Wikipedia for
+            free!
+        </p>
+    </div>
+</div>
+
+<style>
+</style>
