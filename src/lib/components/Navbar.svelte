8fae1493996bafb08f5bf3a199c4cb345a194afb
jakedcolbert <jake.colbert@gordon.edu>
2022-10-23 00:28:21 -0400

    Chapter and Verse Navigation

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -31,23 +31,45 @@ The navbar component.
     /**reference to chapter selector so code can use TabsMenu.setActive*/
     let chapterSelector;
 
+    // Show chapter number on app bar
+    let showChapterNum = $globalConfig.mainFeatures['show-chapter-number-on-app-bar'];
+    // Show chapter menu when the user interacts with the chapter number on the app bar
+    let showChapterMenu = $globalConfig.mainFeatures['show-chapter-selector'];
+    // Show chapter menu after selecting a book from the book menu
+    let showChapterMenuAfterBook = $globalConfig.mainFeatures['show-chapter-selector-after-book'];
+    // Show verse menu after selecting a chapter from the chapter menu
+    let showVerseMenuAfterChapter = $globalConfig.mainFeatures['show-verse-selector'];
+
     /**
      * Pushes reference changes to refs['next']. Pushes final change to default reference.
      */
     function navigateReference(e) {
         if (e.detail.tab === 'Book') {
-            bookSelector.setActive('Chapter');
             refs.set({ book: e.detail.text }, 'next');
+            if (showChapterMenuAfterBook) {
+                bookSelector.setActive('Chapter');
+            } else {
+                $refs = { book: nextRef.book, chapter: 1 };
+                document.activeElement.blur();
+                bookSelector.setActive('Book');
+            }
         } else if (e.detail.tab === 'Chapter') {
-            bookSelector.setActive('Verse');
-            chapterSelector.setActive('Verse');
             refs.set({ chapter: e.detail.text }, 'next');
+            if (showVerseMenuAfterChapter) {
+                bookSelector.setActive('Verse');
+                chapterSelector.setActive('Verse');
+            } else {
+                $refs = { book: nextRef.book, chapter: nextRef.chapter };
+                document.activeElement.blur();
+                bookSelector.setActive('Book');
+                chapterSelector.setActive('Chapter');
+            }
         } else if (e.detail.tab === 'Verse') {
-            bookSelector.setActive('Book');
-            chapterSelector.setActive('Chapter');
             $refs = { book: nextRef.book, chapter: nextRef.chapter };
             // force closes active dropdown elements
             document.activeElement.blur();
+            bookSelector.setActive('Book');
+            chapterSelector.setActive('Chapter');
         }
     }
 
@@ -55,7 +77,6 @@ The navbar component.
     $: books = catalog.find((d) => d.id === nextRef.docSet).documents;
     /**list of chapters in current book*/
     $: chapters = books.find((d) => d.bookCode === nextRef.book).versesByChapters;
-
     onDestroy(unsub);
 </script>
 
@@ -120,30 +141,34 @@ The navbar component.
                 />
             </svelte:fragment>
         </Dropdown>
-        <!-- Chapter Selector -->
-        <Dropdown>
-            <svelte:fragment slot="label">
-                {nextRef.chapter}
-                <DropdownIcon />
-            </svelte:fragment>
-            <svelte:fragment slot="content">
-                <TabsMenu
-                    bind:this={chapterSelector}
-                    options={{
-                        Chapter: {
-                            component: SelectGrid,
-                            props: { options: Object.keys(chapters) }
-                        },
-                        Verse: {
-                            component: SelectGrid,
-                            props: { options: Object.keys(chapters[nextRef.chapter]) }
-                        }
-                    }}
-                    active="Chapter"
-                    on:menuaction={navigateReference}
-                />
-            </svelte:fragment>
-        </Dropdown>
+        {#if showChapterNum}
+            <!-- Chapter Selector -->
+            <Dropdown>
+                <svelte:fragment slot="label">
+                    {nextRef.chapter}
+                    <DropdownIcon />
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    {#if showChapterMenu}
+                        <TabsMenu
+                            bind:this={chapterSelector}
+                            options={{
+                                Chapter: {
+                                    component: SelectGrid,
+                                    props: { options: Object.keys(chapters) }
+                                },
+                                Verse: {
+                                    component: SelectGrid,
+                                    props: { options: Object.keys(chapters[nextRef.chapter]) }
+                                }
+                            }}
+                            active="Chapter"
+                            on:menuaction={navigateReference}
+                        />
+                    {/if}
+                </svelte:fragment>
+            </Dropdown>
+        {/if}
     </div>
     <div class="dy-navbar-end fill-base-content">
         <!-- Mute/Volume Button -->
