7c6c92d52c5630f655f6e3817b90e9e4e2f0ba80
Jason-Lopez8 <Jason.Lopez@gordon.edu>
2022-10-24 12:24:03 -0400

    Action Bar Icons Visibility done

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -20,6 +20,7 @@ The navbar component.
     import { globalConfig } from '$lib/data/stores';
     import { audioActive, refs } from '$lib/data/stores';
     import { onDestroy } from 'svelte';
+    import { globalConfig } from '$lib/data/stores';
 
     let nextRef;
     const unsub = refs.subscribe((v) => {
@@ -82,20 +83,125 @@ The navbar component.
 
 <div class="dy-navbar bg-primary h-full">
     <div class="dy-navbar-start">
-        <label
-            for="sidebar"
-            class="dy-btn dy-btn-ghost p-1 dy-drawer-button {$viewMode === 'Side By Side'
-                ? ''
-                : 'lg:hidden'}"
-        >
-            <HamburgerIcon />
-        </label>
-        <slot name="left-buttons" />
-    </div>
-    <div class="dy-navbar-center">
-        <slot name="center" />
+        <slot name="drawer-button" />
+        <!-- Translation/View Selector -->
+        <Dropdown>
+            <svelte:fragment slot="label">{nextRef.docSet} <DropdownIcon /></svelte:fragment>
+            <svelte:fragment slot="content">
+                <TabsMenu
+                    options={{
+                        'Single Pane': {
+                            tab: { component: SinglePaneIcon },
+                            component: LayoutOptions,
+                            props: { layoutOption: 'Single Pane' }
+                        },
+                        'Side By Side': {
+                            tab: { component: SideBySideIcon },
+                            component: LayoutOptions,
+                            props: { layoutOption: 'Side By Side' }
+                        },
+                        'Verse By Verse': {
+                            tab: { component: VerseByVerseIcon },
+                            component: LayoutOptions,
+                            props: { layoutOption: 'Verse By Verse' }
+                        }
+                    }}
+                    active="Single Pane"
+                />
+            </svelte:fragment>
+        </Dropdown>
+        <!-- Book Selector -->
+        {#if $globalConfig.mainFeatures["book-select"] == "grid" || $globalConfig.mainFeatures["book-select"] =="list" }
+            <Dropdown>
+                <svelte:fragment slot="label">
+                    {nextRef.book}
+                    <DropdownIcon />
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    <TabsMenu
+                        bind:this={bookSelector}
+                        options={{
+                            Book: {
+                                component: SelectGrid,
+                                /**
+                                 * TODO:
+                                 * - add book abbreviations to catalog to be used in UI instead of bookCode
+                                 */
+                                props: { options: books.map((b) => b.bookCode) /*bookAbbreviations*/ }
+                            },
+                            Chapter: {
+                                component: SelectGrid,
+                                props: { options: Object.keys(chapters) }
+                            },
+                            Verse: {
+                                component: SelectGrid,
+                                props: { options: Object.keys(chapters[nextRef.chapter]) }
+                            }
+                        }}
+                        active="Book"
+                        on:menuaction={navigateReference}
+                    />
+                </svelte:fragment>
+            </Dropdown>
+        {/if}
+        <!-- Chapter Selector -->
+        {#if $globalConfig.mainFeatures["show-chapter-numbers"]}
+            <Dropdown>
+                <svelte:fragment slot="label">
+                    {nextRef.chapter}
+                    <DropdownIcon />
+                </svelte:fragment>
+                <svelte:fragment slot="content">
+                    <TabsMenu
+                        bind:this={chapterSelector}
+                        options={{
+                            Chapter: {
+                                component: SelectGrid,
+                                props: { options: Object.keys(chapters) }
+                            },
+                            Verse: {
+                                component: SelectGrid,
+                                props: { options: Object.keys(chapters[nextRef.chapter]) }
+                            }
+                        }}
+                        active="Chapter"
+                        on:menuaction={navigateReference}
+                    />
+                </svelte:fragment>
+            </Dropdown>
+        {/if}
     </div>
     <div class="dy-navbar-end fill-base-content">
-        <slot name="right-buttons" />
+        <!-- Mute/Volume Button -->
+        {#if $globalConfig.mainFeatures["audio-turn-on-at-startup"]} 
+            <button class="dy-btn dy-btn-ghost dy-btn-circle">
+                <label class="dy-swap">
+                    <!-- this hidden checkbox controls the state -->
+                    <input type="checkbox" bind:checked={$audioActive} />
+
+                    <!-- volume on icon -->
+                    <AudioIcon.Volume _class="dy-swap-on fill-black-100" />
+
+                    <!-- volume off icon -->
+                    <AudioIcon.Mute _class="dy-swap-off fill-black-100" />
+                </label>
+                
+            </button>
+        {/if}
+        <!-- Search Button -->
+        {#if $globalConfig.mainFeatures['search']} 
+            <a href="/search" class="dy-btn dy-btn-ghost dy-btn-circle">
+                <SearchIcon />
+            </a>
+        {/if}
+        <!-- Text Appearance Options Menu -->
+        {#if $globalConfig.mainFeatures["text-font-size-slider"] && $globalConfig.mainFeatures["text-line-height-slider"] }
+            <Dropdown>
+                <svelte:fragment slot="label">
+                    <TextAppearanceIcon />
+                </svelte:fragment>
+                <!-- TODO: implement text appearance options -->
+            </Dropdown>
+        {/if}
     </div>
 </div>
