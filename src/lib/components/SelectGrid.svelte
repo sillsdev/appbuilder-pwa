3437ffd2d038400febc377c64c8878a897be3499
jakedcolbert <jake.colbert@gordon.edu>
2022-11-01 11:45:10 -0400

    Removed '_' import

--- src/lib/components/SelectGrid.svelte
+++ src/lib/components/SelectGrid.svelte
@@ -3,7 +3,6 @@
 A component to display menu options in a grid.
 -->
 <script lang="ts">
-    import { _ } from '$env/static/private';
     import { createEventDispatcher } from 'svelte';
     import { globalConfig } from '$lib/data/stores';
     export let options = [''];
