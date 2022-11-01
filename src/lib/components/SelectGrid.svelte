10418899c82032fc45451b30733a19d15704ce64
jakedcolbert <jake.colbert@gordon.edu>
2022-10-31 21:07:33 -0400

    Current Progress

--- src/lib/components/SelectGrid.svelte
+++ src/lib/components/SelectGrid.svelte
@@ -3,11 +3,25 @@
 A component to display menu options in a grid.
 -->
 <script lang="ts">
+    import { _ } from '$env/static/private';
     import { createEventDispatcher } from 'svelte';
+    import { globalConfig } from '$lib/data/stores';
     export let options = [''];
     export let cols = 5;
+
     const dispatch = createEventDispatcher();
     $: rows = Math.ceil(options.length / cols);
+
+    // TODO: Change handling for chapters/verses
+    function bookCollectionColor(bookAbbr: string) {
+      let color = $globalConfig.themes
+        .find((x) => x.name === 'Normal')
+        .colorSets.find((x) => x.type === 'books')
+        .colors[$globalConfig.bookCollections
+        .find((x) => x.id === 'C01')
+        .books.find((x) => x.id === "GEN").section];
+      return color;
+    }
 
     function handleClick(opt: string) {
         dispatch('menuaction', {
@@ -26,6 +40,7 @@ A component to display menu options in a grid.
                         <span
                             on:click={() => handleClick(options[ri * cols + ci])}
                             class="dy-btn dy-btn-square dy-btn-ghost p-0 m-0"
+                              style:background-color={bookCollectionColor(options[ri * cols + ci])}
                             >{options[ri * cols + ci]}</span
                         ></td
                     >
--- src/lib/components/TabsMenu.svelte
+++ src/lib/components/TabsMenu.svelte
@@ -4,11 +4,15 @@ A component to display tabbed menus.
 -->
 <script lang="ts">
     import { createEventDispatcher } from 'svelte';
+    import { globalConfig } from '$lib/data/stores';
 
     export let options: App.TabMenuOptions = { '': { component: '', props: {} } };
     export let active = '';
-    export let _class = 'bg-primary';
     const dispatch = createEventDispatcher();
+
+    $: primaryColor = $globalConfig.themes
+        .find((x) => x.name === 'Normal') // TODO: change to fetch the current theme
+        .colorSets.find((x) => x.type === 'main').colors['PrimaryColor'];
 
     function handleMenuaction({ detail }: CustomEvent) {
         dispatch('menuaction', {
@@ -24,13 +28,15 @@ A component to display tabbed menus.
     };
 </script>
 
-<div class="dy-tabs {_class}">
+<div class="dy-tabs" style:background-color={primaryColor} >
     {#each Object.keys(options) as opt}
         <!-- svelte-ignore a11y-missing-attribute -->
         <!-- svelte-ignore a11y-click-events-have-key-events -->
+        
+        <!-- how to set this to white on dy-tab-active???-->
         <a
             on:click={() => (active = opt)}
-            class="dy-tab dy-tab-bordered {active === opt ? 'dy-tab-active' : ''}"
+            class="dy-tab dy-tab-bordered text-white stroke-white fill-white {active === opt ? 'dy-tab-active' : ''}" 
         >
             {#if options[opt].tab}
                 <svelte:component this={options[opt].tab?.component} {...options[opt].tab?.props} />
