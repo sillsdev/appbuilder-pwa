abfc0cd39f105f7b16c4afa26fc6a79fe0089663
jakedcolbert <jake.colbert@gordon.edu>
2022-11-07 12:21:35 -0500

    format

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -87,14 +87,14 @@ The sidebar/drawer.
         text-decoration: none;
     }
     .fill {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    overflow: hidden
-}
-.fill img {
-    flex-shrink: 0;
-    min-width: 100%;
-    min-height: 100%
-}
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+    }
+    .fill img {
+        flex-shrink: 0;
+        min-width: 100%;
+        min-height: 100%;
+    }
 </style>
