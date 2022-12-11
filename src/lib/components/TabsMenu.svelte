33deb08bf8eb80e0ac415f942d5387992d8e3bc3
jakedcolbert <jake.colbert@gordon.edu>
2022-12-11 04:37:41 -0500

    Format

--- src/lib/components/TabsMenu.svelte
+++ src/lib/components/TabsMenu.svelte
@@ -6,6 +6,7 @@ A component to display tabbed menus.
     import { createEventDispatcher } from 'svelte';
     import { s, convertStyle } from '$lib/data/stores';
 
+    console.log($s);
     export let options: App.TabMenuOptions = { '': { component: '', props: {} } };
     export let active = '';
     const dispatch = createEventDispatcher();
