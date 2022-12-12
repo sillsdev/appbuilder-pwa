492a7c2f776018f2ad603e8f5d79ddc32be56199
jakedcolbert <jake.colbert@gordon.edu>
2022-12-12 11:58:15 -0500

    rebase step 2

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -24,7 +24,7 @@ The navbar component.
         </label>
         <slot name="left-buttons" />
     </div>
-    <div class="dy-navbar-center">
+    <div class="dy-navbar-center" style={convertStyle($s['ui.screen-title'])}>
         <slot name="center" />
     </div>
     <div class="dy-navbar-end fill-base-content">
