88944b08f211b920959e3b82a84ef313b710c24d
jakedcolbert <jake.colbert@gordon.edu>
2023-01-11 16:45:39 -0500

    Format Checkpoint

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -18,36 +18,36 @@
 </script>
 
 <div class="navbar">
-  <Navbar>
-      <!-- Button to close the drawer/sidebar -->
-      <!-- <div slot="left-buttons" /> -->
-      <label for="sidebar" slot="center">
-          <div class="btn btn-ghost normal-case text-xl">{$t['Menu_Layout']}</div>
-      </label>
-      <!-- <div slot="right-buttons" /> -->
-  </Navbar>
+    <Navbar>
+        <!-- Button to close the drawer/sidebar -->
+        <!-- <div slot="left-buttons" /> -->
+        <label for="sidebar" slot="center">
+            <div class="btn btn-ghost normal-case text-xl">{$t['Menu_Layout']}</div>
+        </label>
+        <!-- <div slot="right-buttons" /> -->
+    </Navbar>
 </div>
 
 <div style:margin-left="auto" style:margin-right="auto">
-  <TabsMenu
-      options={{
-          'Single Pane': {
-              tab: { component: SinglePaneIcon },
-              component: LayoutOptions,
-              props: { layoutOption: 'Single Pane' }
-          },
-          'Side By Side': {
-              tab: { component: SideBySideIcon },
-              component: LayoutOptions,
-              props: { layoutOption: 'Side By Side' }
-          },
-          'Verse By Verse': {
-              tab: { component: VerseByVerseIcon },
-              component: LayoutOptions,
-              props: { layoutOption: 'Verse By Verse' }
-          }
-      }}
-      active="Single Pane"
-      on:menuaction={navigateReference}
-  />
-</div>  
+    <TabsMenu
+        options={{
+            'Single Pane': {
+                tab: { component: SinglePaneIcon },
+                component: LayoutOptions,
+                props: { layoutOption: 'Single Pane' }
+            },
+            'Side By Side': {
+                tab: { component: SideBySideIcon },
+                component: LayoutOptions,
+                props: { layoutOption: 'Side By Side' }
+            },
+            'Verse By Verse': {
+                tab: { component: VerseByVerseIcon },
+                component: LayoutOptions,
+                props: { layoutOption: 'Verse By Verse' }
+            }
+        }}
+        active="Single Pane"
+        on:menuaction={navigateReference}
+    />
+</div>
