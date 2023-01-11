bc2aae4a8661e2aa8a6fad86949fd859492ccc78
jakedcolbert <jake.colbert@gordon.edu>
2023-01-11 16:27:55 -0500

    Added layout route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,18 +6,18 @@
     import CollectionSelector from '$lib/components/CollectionSelector.svelte';
     import ScrolledContent from '$lib/components/ScrolledContent.svelte';
     import { audioActive, refs, themeColors } from '$lib/data/stores';
-    import { AudioIcon, SearchIcon } from '$lib/icons';
+    import { AudioIcon, BibleIcon, SearchIcon } from '$lib/icons';
     import Navbar from '$lib/components/Navbar.svelte';
     import TextAppearanceSelector from '$lib/components/TextAppearanceSelector.svelte';
     import config from '$lib/data/config';
 
     const showSearch = config.mainFeatures['search'];
+    const showCollections = config.bookCollections.length > 1;
 </script>
 
 <div class="navbar">
     <Navbar>
         <div slot="left-buttons">
-            <CollectionSelector />
             <BookSelector />
             <ChapterSelector />
         </div>
@@ -45,6 +45,11 @@
             {/if}
             <!-- Text Appearance Options Menu -->
             <TextAppearanceSelector />
+            {#if showCollections}
+                <a href="/layout" class="dy-btn dy-btn-ghost dy-btn-circle">
+                    <BibleIcon color="white" />
+                </a>
+            {/if}
         </div>
     </Navbar>
 </div>
