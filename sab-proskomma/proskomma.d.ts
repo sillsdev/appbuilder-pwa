7084292d15633f588d4994aa661c238415c92cd9
davidmoore1 <david_moore1@sil.org>
2023-01-27 15:57:20 -0500

    Create prototype for Proskomma method

--- sab-proskomma/proskomma.d.ts
+++ sab-proskomma/proskomma.d.ts
@@ -1,5 +1,6 @@
 declare module 'proskomma' {
     export class Proskomma {
+        gqlQuerySync(query: string, callback?: (r: any) => void): any;
         gqlQuery(query: string, callback?: (r: any) => void): Promise<any>;
         validateSelectors(): void;
     }
