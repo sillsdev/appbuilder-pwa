7ebce591e9f0171bfa704e89b615ea32f66ddd96
Chris Hubbard <chris@thehubbards.org>
2023-02-02 15:05:40 -0500

    Add more audio icons (#114)
    Use https://vectorizer.ai to convert png icons to svg

--- src/lib/icons/audio/index.js
+++ src/lib/icons/audio/index.js
@@ -2,20 +2,32 @@ import FastForwardIcon from './FastForwardIcon.svelte';
 import MuteIcon from './MuteIcon.svelte';
 import PauseIcon from './PauseIcon.svelte';
 import PlayIcon from './PlayIcon.svelte';
+import PlayRepeatIcon from './PlayRepeatIcon.svelte';
+import RepeatOneIcon from './RepeatOneIcon.svelte';
+import RepeatIcon from './RepeatIcon.svelte';
+import RepeatOffIcon from './RepeatOffIcon.svelte';
+import RepeatOffStopIcon from './RepeatOffStopIcon.svelte';
 import RewindIcon from './RewindIcon.svelte';
 import SkipNextIcon from './SkipNextIcon.svelte';
 import SkipPreviousIcon from './SkipPreviousIcon.svelte';
 import VolumeIcon from './VolumeIcon.svelte';
+import SpeedIcon from './SpeedIcon.svelte';
 
 const AudioIcon = {
     Mute: MuteIcon,
     Volume: VolumeIcon,
     Pause: PauseIcon,
     Play: PlayIcon,
+    PlayRepeat: PlayRepeatIcon,
     FF: FastForwardIcon,
     RW: RewindIcon,
     Skip: SkipNextIcon,
-    Prev: SkipPreviousIcon
+    Prev: SkipPreviousIcon,
+    RepeatOne: RepeatOneIcon,
+    Repeat: RepeatIcon,
+    RepeatOff: RepeatOffIcon,
+    RepeatOffStop: RepeatOffStopIcon,
+    Speed: SpeedIcon
 };
 
 export { AudioIcon };
