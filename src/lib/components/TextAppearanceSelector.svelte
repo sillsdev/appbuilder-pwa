8c9a51864b81c183ee0a5d04953d5c8461c024b0
Jason-Lopez8 <Jason.Lopez@gordon.edu>
2023-02-18 16:14:01 -0500

    Added buttons that work

--- src/lib/components/TextAppearanceSelector.svelte
+++ src/lib/components/TextAppearanceSelector.svelte
@@ -1,13 +1,15 @@
 <!--
 @component
 The navbar component.
+TODO
+- Add functionality to sliders
+- Add icons to sliders as well and a font size on the end.
 -->
 <script>
     import Dropdown from './Dropdown.svelte';
     import { TextAppearanceIcon } from '$lib/icons';
-    import { language, languages, theme, themes } from '$lib/data/stores';
+    import { language, languages, theme, themes, s, convertStyle } from '$lib/data/stores';
     import config from '$lib/data/config';
-    import TabsMenu from './TabsMenu.svelte';
 
     const countThemes = config.themes.filter((x) => x.enabled).length;
 
@@ -34,34 +36,67 @@ The navbar component.
         console.log(event);
         if (event.shiftKey) {
             rotateLanguages();
-        } else {
-            rotateThemes();
         }
     };
+
     const rotateLanguages = () => {
         $language = rotate(languages, $language);
     };
 
-    const rotateThemes = () => {
-        $theme = rotate(themes, $theme);
+    const handleNormalClick = () => {
+        $theme = themes[0];
     };
 
-    // <button class="dy-btn dy-btn-ghost dy-btn-circle">
+    const handleSepiaClick = () => {
+        $theme = themes[1];
+    };
+
+    const handleDarkClick = () => {
+        $theme = themes[2];
+    };
 </script>
 
 <!-- TextAppearanceSelector -->
 {#if showTextAppearence}
-<div class="dy-dropdown dy-dropdown-end">
-    <Dropdown>
-        <svelte:fragment slot="label">
-            <TextAppearanceIcon color="white" />
-        </svelte:fragment>
-        <svelte:fragment slot="content">
-            <!-- Sliders for when text appearence text size is implemented place holder no functionality-->
-            <input type="range" min="0" max="100" value="60" class="dy-range dy-range-xs" />
-            <input type="range" min="0" max="100" value="60" class="dy-range dy-range-xs" />
-            <!-- Theme Selction buttons-->
-        </svelte:fragment>
-    </Dropdown>
-</div>
+    <div class="dy-dropdown dy-dropdown-end">
+        <Dropdown>
+            <svelte:fragment slot="label">
+                <TextAppearanceIcon color="white" />
+            </svelte:fragment>
+            <svelte:fragment slot="content">
+                <!-- Sliders for when text appearence text size is implemented place holder no functionality-->
+                <input type="range" min="0" max="100" value="60" class="dy-range dy-range-xs" />
+                <input type="range" min="0" max="100" value="60" class="dy-range dy-range-xs" />
+                <!-- Theme Selction buttons-->
+                <div class="wrapper">
+                    <button
+                        class="dy-btn "
+                        style={convertStyle($s['ui.primarycolor'])}
+                        on:click={handleNormalClick}
+                    />
+                    <button
+                        class="dy-btn "
+                        style={convertStyle($s['ui.'])}
+                        on:click={handleSepiaClick}
+                    />
+                    <button
+                        class="dy-btn "
+                        style={convertStyle($s['ui.'])}
+                        on:click={handleDarkClick}
+                    />
+                </div>
+            </svelte:fragment>
+        </Dropdown>
+    </div>
 {/if}
+
+<style>
+    div.wrapper {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        gap: 8px;
+        padding: 0.5em 0;
+        margin-left: 8px;
+        margin-right: 8px;
+    }
+</style>
