6af5f4e45ebc86b4d90cecf1fa7be44d511cfa86
Chris Hubbard <chris@thehubbards.org>
2023-03-03 16:50:47 -0500

    Add userSettings store

--- src/lib/data/stores.js
+++ src/lib/data/stores.js
@@ -100,6 +100,18 @@ export const s = derived(themeColors, $themeColors => {
     }, {});
 });
 
+function defaultUserSettings() {
+    const defaultSettings = {
+        'verse-numbers' : config.mainFeatures['show-verse-numbers'],
+        'verse-layout' : config.mainFeatures['verse-layout'],
+        'verse-of-the-day-time' : config.mainFeatures['verse-of-the-day-time'],
+    };
+    return defaultSettings;
+}
+setDefaultStorage('userSettings', JSON.stringify(defaultUserSettings()));
+export const userSettings = writable(JSON.parse(localStorage.userSettings));
+userSettings.subscribe(value => localStorage.userSettings = JSON.stringify(value));
+
 /**a group of writable stores to store the top visible verse in a group*/
 export const scrolls = groupStore(writable, 'title');
 /**the current view/layout mode*/
