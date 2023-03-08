9cc1048c8305617b999bd6d6760354e1d4c54d89
Chris Hubbard <chris@thehubbards.org>
2023-03-08 16:10:42 -0500

    Implement Setting bindings
    * move initUserPreferenceSettings to data/stores/settings.ts
    * get defaults for userSettings from userPreferenceSettings
    * add mergeDefaultStorage to handle when new defaults come in
    * add bind:value on settings/+page.svelte to userSettings
    * use userSettings for Grid vs. List in BookSelector
    * use userSettings for showVerseSelector in BookSelector and
      ChapterSelector
    * use userSettings for language

--- src/lib/data/stores/localization.js
+++ src/lib/data/stores/localization.js
@@ -1,13 +1,11 @@
-import { writable, derived } from 'svelte/store';
-import { setDefaultStorage } from './storage';
+import { derived } from 'svelte/store';
+import { userSettings } from './setting';
 import config from '../config';
 
 /** localization */
 export const languageDefault = config.translationMappings.defaultLang;
-setDefaultStorage('language', languageDefault);
 export const languages = Object.keys(config.interfaceLanguages.writingSystems);
-export const language = writable(localStorage.language);
-language.subscribe(value => localStorage.language = value);
+export const language = derived(userSettings, $userSettings => $userSettings['interface-language']);
 
 export const t = derived(language, $language => {
     return Object.keys(config.translationMappings.mappings).reduce((mappings, key) => {
