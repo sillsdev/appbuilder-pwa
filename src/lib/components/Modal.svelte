b23aca59aa4623df50cbbdbd95cc395f271b47e1
jakedcolbert <jake.colbert@gordon.edu>
2023-03-17 14:46:42 -0400

    Modal file added

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,17 @@
+<!--
+@component
+A simple modal component from DaisyUI. Closes when clicked outside of.
+
+See https://daisyui.com/components/modal/#modal-that-closes-when-clicked-outside
+-->
+
+<!-- The button to open modal -->
+<label for="my-modal-4" class="btn"><slot name="label" /></label>
+
+<!-- Put this part before </body> tag -->
+<input type="checkbox" id="my-modal-4" class="modal-toggle" />
+<label for="my-modal-4" class="modal cursor-pointer">
+  <label class="modal-box relative" for="">
+    <slot name="content" />
+  </label>
+</label>
