16321aec00e837767ceb29bc68ac77f058632536
jakedcolbert <jake.colbert@gordon.edu>
2023-03-20 17:38:30 -0400

    Added background color support.

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -5,7 +5,9 @@ A simple modal component from DaisyUI. Closes when clicked outside of.
 See https://daisyui.com/components/modal/#modal-that-closes-when-clicked-outside
 -->
 <script>
+    import { s, convertStyle } from '$lib/data/stores';
     export let id;
+    $: console.log($s['ui.background']);
 </script>
 
 <!-- The button to open modal -->
@@ -14,7 +16,7 @@ See https://daisyui.com/components/modal/#modal-that-closes-when-clicked-outside
 <!-- Put this part before </body> tag -->
 <input type="checkbox" id={id} class="dy-modal-toggle" />
 <label for={id} class="dy-modal cursor-pointer">
-    <label class="dy-modal-box relative" for="">
+    <label style={convertStyle($s['ui.background'])} class="dy-modal-box relative" for="">
         <slot name="content" />
     </label>
 </label>
