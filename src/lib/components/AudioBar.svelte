88aaba5083c9067399bfffffcf4d4172362edbb9
Elijah Opoku-Nyarko <elijah.opoku-nyarko@gordon.edu>
2023-03-23 12:00:26 -0400

    Added Playback Speed to a separate component

--- src/lib/components/AudioBar.svelte
+++ src/lib/components/AudioBar.svelte
@@ -7,26 +7,9 @@ TODO:
 -->
 <script>
     import { AudioIcon } from '$lib/icons';
-    import { refs, audioHighlight, audioActive, s, playMode, theme } from '$lib/data/stores';
+    import { refs, audioHighlight, audioActive, s, playMode } from '$lib/data/stores';
+    import AudioPlayback from './AudioPlayback.svelte';
     import config from '$lib/data/config';
-    import { createEventDispatcher } from 'svelte';
-
-    const dispatch = createEventDispatcher();
-
-    let isPopupOpen = false;
-
-    function openPopup() {
-        isPopupOpen = true;
-    }
-
-    function closePopup() {
-        isPopupOpen = false;
-    }
-
-    function setPlaySpeed(event) {
-        const speed = parseFloat(event.target.value);
-        audio.playbackRate = speed;
-    }
 
     let duration = NaN;
     let progress = 0;
@@ -35,7 +18,7 @@ TODO:
     let playAfterSkip = false;
     let timeIndex = 0;
     let timing = [];
-    /**@type{HTMLAudioElement}*/ let audio;
+    /**@type{HTMLAudioElement}*/ export let audio;
 
     //get the audio source and timing files, based off the current reference
     const getAudio = async (collection, book, chapter) => {
@@ -194,12 +177,13 @@ TODO:
     const showRepeatMode = config.mainFeatures['audio-repeat-mode-button'];
     const playIconSize = config.mainFeatures['audio-play-button-size'] === 'normal' ? '24' : '48';
     const playIcon = playIconOptons[config.mainFeatures['audio-play-button-style']];
-    $: themeColor = $s['ui.bar.action']['background-color'];
     $: iconColor = $s['ui.bar.audio.icon']['color'];
     $: backgroundColor = $s['ui.bar.audio']['background-color'];
-    $: dialogBackgroundColor = $s['ui.dialog']['background-color'];
     $: audioBarClass = $refs.hasAudio?.timingFile ? 'audio-bar' : 'audio-bar-progress';
     $: mayResetPlayMode($refs.hasAudio?.timing);
+    $: audioPlaybackProps = { audio };
+
+    let audioPlaybackProps = {};
 </script>
 
 <div class={audioBarClass} style:background-color={backgroundColor}>
@@ -256,66 +240,7 @@ TODO:
     </div>
     <div class="dy-button-group audio-speed">
         {#if showSpeed}
-            <button class="dy-btn-sm dy-btn-ghost" on:click={openPopup}>
-                <AudioIcon.Speed color={iconColor} />
-            </button>
-        {/if}
-        {#if isPopupOpen}
-            <div class="popup">
-                <!-- svelte-ignore a11y-click-events-have-key-events -->
-                <div class="overlay" on:click={closePopup} />
-                <div
-                    class="popup-content"
-                    class:dark={$theme === 'Dark'}
-                    style="background-color:{dialogBackgroundColor};"
-                >
-                    <slot />
-                    <h1>
-                        <b style="">Playback Speed</b>
-                    </h1>
-                    <div class="speed-controls">
-                        <label>
-                            <input type="radio" name="speed" value="0.4" on:click={setPlaySpeed} />
-                            0.4x
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="0.6" on:click={setPlaySpeed} />
-                            0.6x
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="0.7" on:click={setPlaySpeed} />
-                            0.7x
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="0.8" on:click={setPlaySpeed} />
-                            0.8x
-                        </label>
-                        <label>
-                            <input
-                                type="radio"
-                                name="speed"
-                                value="1"
-                                on:click={setPlaySpeed}
-                                checked
-                            />
-                            Normal
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="1.2" on:click={setPlaySpeed} />
-                            1.2x
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="1.4" on:click={setPlaySpeed} />
-                            1.4x
-                        </label>
-                        <label>
-                            <input type="radio" name="speed" value="1.6" on:click={setPlaySpeed} />
-                            1.6x
-                        </label>
-                    </div>
-                    <button on:click={closePopup}>Close</button>
-                </div>
-            </div>
+            <AudioPlayback {...audioPlaybackProps} />
         {/if}
     </div>
     {#if !$refs.hasAudio.timingFile}
@@ -331,9 +256,6 @@ TODO:
 </div>
 
 <style>
-    div.dark {
-        color: #e0e0e0;
-    }
     .audio-bar {
         display: grid;
         grid-auto-columns: 3.125rem auto 3.125rem;
@@ -364,7 +286,6 @@ TODO:
         grid-column: 1;
         place-self: center;
     }
-
     .audio-controls {
         grid-row: 1;
         grid-column: 2;
@@ -375,51 +296,5 @@ TODO:
         grid-row: 1;
         grid-column: 3;
         place-self: center;
-    }
-    .popup {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        z-index: 9999;
-    }
-
-    .overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
-    }
-
-    .popup-content {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 90%;
-        height: 90%;
-        max-width: 700px;
-        max-height: 500px;
-        padding: 20px;
-        border-radius: 5px;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
-
-    .speed-controls {
-        align-items: left;
-        margin-top: 10px;
-        display: flex;
-        flex-direction: column;
-    }
-
-    label {
-        margin-right: 0px;
-        margin-top: 10px;
-        margin-bottom: 10px;
     }
 </style>
