774c002d9052478e19985c735a94b2acdf6eaf04
davidmoore1 <david_moore1@sil.org>
2023-04-06 14:19:42 -0400

    Remove commented out console log statements in annotations.

--- src/lib/data/stores/annotation.js
+++ src/lib/data/stores/annotation.js
@@ -6,19 +6,16 @@ setDefaultStorage('bookmarks', '[]');
 export const bookmarks = writable(JSON.parse(localStorage.bookmarks));
 bookmarks.subscribe(value => {
     localStorage.bookmarks = JSON.stringify(value);
-    //console.log("Bookmarks", value);
 });
 
 setDefaultStorage('highlights', '[]');
 export const highlights = writable(JSON.parse(localStorage.highlights));
 highlights.subscribe(value => {
     localStorage.notes = JSON.stringify(value);
-    //console.log("Highlights", value);
 })
 
 setDefaultStorage('notes', '[]');
 export const notes = writable(JSON.parse(localStorage.notes));
 notes.subscribe(value => {
     localStorage.notes = JSON.stringify(value);
-    //console.log("Notes", value);
 })
