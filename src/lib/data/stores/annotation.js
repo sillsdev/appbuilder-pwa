a86250ba94bb309b025b2bd4e8e035b19f12b89e
davidmoore1 <david_moore1@sil.org>
2023-04-06 14:05:25 -0400

    Pull initial annotation values from localStorage if they exist

--- src/lib/data/stores/annotation.js
+++ src/lib/data/stores/annotation.js
@@ -1,22 +1,23 @@
 import { writable } from "svelte/store";
+import { setDefaultStorage } from "./storage";
 
 /* list of bookmarks */
-export const bookmarks = writable([]);
-
+setDefaultStorage('bookmarks', '[]');
+export const bookmarks = writable(JSON.parse(localStorage.bookmarks));
 bookmarks.subscribe(value => {
     localStorage.bookmarks = JSON.stringify(value);
     //console.log("Bookmarks", value);
 });
 
-export const highlights = writable([]);
-
+setDefaultStorage('highlights', '[]');
+export const highlights = writable(JSON.parse(localStorage.highlights));
 highlights.subscribe(value => {
     localStorage.notes = JSON.stringify(value);
     //console.log("Highlights", value);
 })
 
-export const notes = writable([]);
-
+setDefaultStorage('notes', '[]');
+export const notes = writable(JSON.parse(localStorage.notes));
 notes.subscribe(value => {
     localStorage.notes = JSON.stringify(value);
     //console.log("Notes", value);
