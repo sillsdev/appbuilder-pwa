f2a806791e72de638d212b9d4e75ebe4a16074bf
Elijah Opoku-Nyarko <78819149+elijahbigk77@users.noreply.github.com>
2023-04-11 08:56:33 -0400

    Audio advances to any specific location on the progress bar when that location is clicked (#178)

--- src/lib/components/AudioBar.svelte
+++ src/lib/components/AudioBar.svelte
@@ -176,6 +176,15 @@ TODO:
         }
     }
 
+    function seekAudio(event) {
+        if (!loaded) return;
+        // Calculate the percentage of the progress bar that was clicked
+        const progressBar = document.getElementById('progress-bar');
+        const percent = (event.clientX - progressBar.offsetLeft) / progressBar.offsetWidth;
+        // Set the current time of the audio element to the corresponding time based on the percent
+        audio.currentTime = duration * percent;
+    }
+
     const playIconOptons = {
         arrow: AudioIcon.Play,
         'filled-circle': AudioIcon.PlayFillCircle,
@@ -261,7 +270,14 @@ TODO:
         <!-- Progress Bar -->
         <div class="audio-progress-value">{duration ? format(progress) : ''}</div>
         {#if loaded}
-            <progress class="dy-progress audio-progress" value={progress} max={duration} />
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <progress
+                id="progress-bar"
+                class="dy-progress audio-progress"
+                value={progress}
+                max={duration}
+                on:click={seekAudio}
+            />
         {:else}
             <progress class="dy-progress audio-progress" value="0" max="1" />
         {/if}
