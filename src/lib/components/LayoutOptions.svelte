cc8300313ca9a62776c83c939b8dac6d2fb4da1c
jakedcolbert <jake.colbert@gordon.edu>
2023-06-01 15:22:06 -0400

    SideBySide dropdown menu labels

--- src/lib/components/LayoutOptions.svelte
+++ src/lib/components/LayoutOptions.svelte
@@ -6,6 +6,7 @@ TODO:
 -->
 <script lang="ts">
     import { onDestroy, createEventDispatcher } from 'svelte';
+    import SelectList from './SelectList.svelte';
     import Dropdown from './Dropdown.svelte';
     import { DropdownIcon } from '$lib/icons';
     import { catalog } from '$lib/data/catalog';
@@ -16,15 +17,17 @@ TODO:
     const dispatch = createEventDispatcher();
 
     let nextDocSet;
-    console.log(config.bookCollections);
 
-    const docSetList = catalog.map((ds) => ds.id);
-    const allowSinglePane = config.bookCollections.map((ds) => ({
+    //const allDocSets = catalog.map((ds) => ds.id);
+    const allDocSets = config.bookCollections.map((ds) => ({
         id: ds.languageCode + '_' + ds.id,
         name: ds.collectionName,
         singlePane: ds.features['bc-allow-single-pane'],
         description: ds?.collectionDescription
     }));
+
+    $: leftSide = allDocSets.find((x) => x.id === $refs.docSet);
+    $: rightSide = allDocSets.filter((x) => x.id != $refs.docSet)[0];
 
     const removeKey = refs.subscribe((v) => {
         nextDocSet = v.docSet;
@@ -38,6 +41,8 @@ TODO:
             text: opt
         });
     }
+
+    $: console.log(allDocSets.filter((x) => x.id != $refs.docSet).map((x)=> ({ label: x.name, id: x.id })));
 </script>
 
 <div class="p-2">
@@ -47,7 +52,7 @@ TODO:
             <strong>{$t['Layout_Single_Pane']}</strong>
         </p>
         <ul class="dy-menu dy-menu-compact mx-auto">
-            {#each allowSinglePane.filter((x) => x.singlePane === true) as d}
+            {#each allDocSets.filter((x) => x.singlePane === true) as d}
                 <!-- svelte-ignore a11y-missing-attribute -->
                 <li>
                     <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -84,10 +89,14 @@ TODO:
                 <Dropdown>
                     <svelte:fragment slot="label">
                         <div class="px-3">1.</div>
-                        <div class="normal-case">Dropdown</div>
+                        <div class="normal-case">{leftSide.name}</div>
                         <DropdownIcon />
                     </svelte:fragment>
-                    <svelte:fragment slot="content" />
+                    <svelte:fragment slot="content">
+                      <!-- <SelectList options={{
+                        rows: allDocSets.filter((x) => x.id != $refs.docSet).map((x) => [id: x.id, label: x.name])
+                      }}/> -->
+                    </svelte:fragment>
                 </Dropdown>
             </li>
             <li>
@@ -95,14 +104,15 @@ TODO:
                 <Dropdown>
                     <svelte:fragment slot="label">
                         <div class="px-3">2.</div>
-                        <div class="normal-case">Dropdown</div>
+                        <div class="normal-case">{rightSide.name}</div>
                         <DropdownIcon />
                     </svelte:fragment>
-                    <svelte:fragment slot="content" />
+                    <svelte:fragment slot="content">
+
+                    </svelte:fragment>
                 </Dropdown>
             </li>
         </ul>
-
         <!-- Verse By Verse -->
     {:else if layoutOption === 'Verse By Verse'}
         <p style:color={$themeColors['LayoutTitleColor']}>
