18401496ede562d74e9d946d91d281e61466db0e
davidbbryant <123591459+davidbbryant@users.noreply.github.com>
2023-07-11 15:31:23 -0400

    Implement annotation sorting
    Implement sorting for the bookmarks, highlights, and annotations pages. Pages also now automatically sort by date when opened.

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
     import IconCard from '$lib/components/IconCard.svelte';
+    import SortMenu from '$lib/components/SortMenu.svelte';
     import { NoteIcon } from '$lib/icons';
     import Navbar from '$lib/components/Navbar.svelte';
-    import { t, notes, monoIconColor } from '$lib/data/stores';
+    import { t, monoIconColor } from '$lib/data/stores';
     import { formatDate } from '$lib/scripts/dateUtils.js';
+    import { page } from '$app/stores';
 
     function handleMenuaction(event: CustomEvent, id: string) {
         switch (event.detail.text) {
             case $t['Annotation_Menu_View']:
-                console.log('View: ', $notes[id].reference);
+                console.log('View: ', notes[id].reference);
                 break;
             case $t['Annotation_Menu_Share']:
-                console.log('Share: ', $notes[id].reference);
+                console.log('Share: ', notes[id].reference);
                 break;
             case $t['Annotation_Menu_Delete']:
                 console.log('Delete: ', id);
@@ -19,7 +21,57 @@
         }
     }
 
-    export let data;
+    function handleSortAction(event: CustomEvent) {
+        switch (event.detail.text) {
+            case $t['Annotation_Sort_Order_Reference']:
+                sortByReference();
+                break;
+            case $t['Annotation_Sort_Order_Date']:
+                sortByDate();
+                break;
+        }
+    }
+
+    function sortByReference() {
+        notes.sort((a, b) => {
+            if (a.bookIndex > b.bookIndex) {
+                return 1;
+            }
+            else if (a.bookIndex < b.bookIndex) {
+                return -1;
+            }
+            else if (parseInt(a.chapter) > parseInt(b.chapter)) {
+                return 1;
+            }
+            else if (parseInt(a.chapter) < parseInt(b.chapter)) {
+                return -1;
+            }
+            else if (parseInt(a.verse) > parseInt(b.verse)) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        })
+
+        notes = notes
+    }
+
+    function sortByDate() {
+        notes.sort((a, b) => {
+            if (a.date < b.date) {
+                return 1;
+            }
+            else {
+                return -1;
+            }
+        })
+
+        notes = notes;
+    }
+    
+    let notes = $page.data.notes;
+    sortByDate();
 </script>
 
 <div class="grid grid-rows-[auto,1fr]" style="height:100vh;height:100dvh;">
@@ -29,12 +81,24 @@
             <label for="sidebar" slot="center">
                 <div class="btn btn-ghost normal-case text-xl">{$t['Annotation_Notes']}</div>
             </label>
+
+            <!-- svelte-ignore a11y-label-has-associated-control -->
+            <label slot="right-buttons">
+                {@const sortMenu = {
+                    actions: [
+                        $t['Annotation_Sort_Order_Reference'],
+                        $t['Annotation_Sort_Order_Date']
+                    ]
+                }}
+                <SortMenu on:menuaction={(e) => handleSortAction(e)} {...sortMenu}>
+                </SortMenu>
+            </label>
             <!-- <div slot="right-buttons" /> -->
         </Navbar>
     </div>
 
     <div class="overflow-y-auto">
-        {#each data.notes as n}
+        {#each notes as n}
             {@const iconCard = {
                 collection: n.collection,
                 reference: n.reference,
