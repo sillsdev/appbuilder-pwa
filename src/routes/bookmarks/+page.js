264d3d81d26dc30d55de3eb2125b146c2aa5a259
Chris Hubbard <chris@thehubbards.org>
2023-07-17 11:30:04 -0400

    Enable deleting annotations (#4)
    * Enable deleting annotations
    
    * change display of annotations to be data-driven on sort order
    * change load of annotations to mark depends and invalidate on delete
    
    * Add compareColor

--- src/routes/bookmarks/+page.js
+++ src/routes/bookmarks/+page.js
@@ -1,7 +1,8 @@
 import { getBookmarks } from '$lib/data/bookmarks';
 
 /** @type {import('./$types').PageLoad} */
-export async function load() {
+export async function load({ depends }) {
     const bookmarks = await getBookmarks();
+    depends('bookmarks');
     return { bookmarks };
 }
