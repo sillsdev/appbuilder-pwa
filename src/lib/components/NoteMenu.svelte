7454818836cba03c76442acb9e7104561ce77a94
jakedcolbert <jake.colbert@gordon.edu>
2023-07-20 14:11:15 -0400

    Created notes menu with text box and confirm/save btns

--- src/lib/components/NoteMenu.svelte
+++ src/lib/components/NoteMenu.svelte
@@ -1,14 +1,26 @@
 <script>
     import Modal from './Modal.svelte';
+    import { t } from '$lib/data/stores';
     let id = 'note';
     let modal;
     export function showModal() {
         modal.showModal();
     }
+
+    let noteText;
 </script>
 
 <Modal bind:this={modal} {id} useLabel={false}>
     <svelte:fragment slot="content">
-        <div>test</div>
+        <div id="container" class="flex flex-col justify-evenly">
+            <div class="annotation-item-title w-full pb-3">{$t['Annotation_Note_Add']}</div>
+            <div>
+                <textarea bind:this={noteText} class="dy-textarea w-full" />
+            </div>
+            <div class="w-full flex justify-end mt-4">
+                <button class="dy-btn dy-btn-sm dy-btn-ghost">delete</button>
+                <button class="dy-btn dy-btn-sm dy-btn-ghost">save</button>
+            </div>
+        </div>
     </svelte:fragment>
 </Modal>
