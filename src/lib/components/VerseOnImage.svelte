4f1b9f6c4d7213f80af4d0d0cb1ef81291f056d7
Cahleeb <calebchristopherparks@gmail.com>
2023-07-24 13:51:01 -0700

    Implemented scroll-snap functionality for editors.

--- src/lib/components/VerseOnImage.svelte
+++ src/lib/components/VerseOnImage.svelte
@@ -15,6 +15,7 @@ The verse on image component.
         themeColors,
         selectedVerses
     } from '$lib/data/stores';
+    import { base } from '$app/paths';
     import config from '$lib/data/config';
 
     $: barColor = $themeColors['SliderBarColor'];
@@ -24,11 +25,29 @@ The verse on image component.
     $: verses = $selectedVerses.reduce((text, v) => (text += v.text), '');
     let cnv;
 
+    let cnv_text = $selectedVerses.reduce((text, v) => (text += v.text), '');
+    let cnv_font = '30px Comic Sans MS';
+    let cnv_color = 'red';
+    // $: cnv_font_w = cnv.width / 2;
+    // $: cnv_font_h = cnv.height / 2;
+
     onMount(() => {
-        const canvas = cnv.getContext('2d');
-        canvas.fillStyle = 'red';
-        canvas.fillRect(0, 0, cnv.width, cnv.height);
-        // put verses here
+        const context = cnv.getContext('2d');
+
+        // /*DEBUG*/ context.fillStyle = 'yellow';
+        // /*DEBUG*/ context.fillRect(0, 0, cnv.width, cnv.height);
+
+        var background = new Image();
+        background.src = base + '/backgrounds/aaron-burden-6jYoil2GhVk-unsplash-1080.jpg';
+
+        // Make sure the image is loaded first otherwise nothing will draw.
+        background.onload = function () {
+            context.drawImage(background, 0, 0, cnv.width, cnv.height);
+            context.font = cnv_font;
+            context.fillStyle = cnv_color;
+            context.textAlign = 'center';
+            context.fillText(cnv_text, cnv.width / 2, cnv.height / 2);
+        };
     });
 
     function shareCanvas() {
@@ -54,10 +73,46 @@ The verse on image component.
             }
         });
     }
+
+    let active_editor_index = -1;
+
+    function centerButton(n) {
+        const container = document.getElementById('editorTabs');
+        const containerRect = container.getBoundingClientRect();
+        const buttons = document.querySelectorAll('#editorTabs button');
+        const button = n >= 0 && n < buttons.length ? buttons[n] : null;
+        const buttonRect = button.getBoundingClientRect();
+
+        const scrollOffset =
+            buttonRect.left - containerRect.left - containerRect.width / 2 + buttonRect.width / 2;
+
+        container.scrollBy({ left: scrollOffset, behavior: 'smooth' });
+
+        let carousel = document.getElementById('editorsPane');
+        const editors = carousel.children; // document.querySelectorAll('#editorsPane div');// Get the carousel's children.
+        if (n < editors.length) {
+            const itemWidth = editors[0].offsetWidth; // Calculate the width of each carousel item
+
+            const scrollDistance = n * itemWidth; // Calculate the total scroll distance to reach the nth child
+
+            carousel.scrollTo({ left: scrollDistance, behavior: 'smooth' }); // Scroll the carousel to the desired position
+        } else {
+            console.error('Invalid child index');
+        }
+
+        // Remove the activeButton class from all buttons
+        buttons.forEach((btn) => {
+            btn.classList.remove('activeButton');
+        });
+
+        // Set the clicked button as the activeButton
+        active_editor_index = n;
+        button.classList.add('activeButton');
+    }
 </script>
 
 <div class="flex flex-col flex-nowrap" style="">
-    <div id="verseOnImgPreview" style="border: 2px solid red; width:auto;">
+    <div id="verseOnImgPreview" style="border: 2px solid red; width:full;">
         <!-- Preview display of the image and text -->
         <canvas bind:this={cnv} width="300" height="full" style="border: 2px solid green;" />
         <button on:click={shareCanvas}>Export Canvas</button>
@@ -69,44 +124,123 @@ The verse on image component.
         style="border: 2px solid teal; overflow-x:scroll;"
     >
         <!-- NavBar of tab buttons to bring up the different editor panes -->
-        <button class="dy-btn-sm dy-btn-ghost">
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(0)}
+            class:activeButton={active_editor_index == 0}
+        >
             <TextAppearanceIcon color="black" />
         </button>
-        <button class="dy-btn-sm dy-btn-ghost">
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(1)}
+            class:activeButton={active_editor_index == 1}
+        >
+            <TextAppearanceIcon color="red" />
+        </button>
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(2)}
+            class:activeButton={active_editor_index == 2}
+        >
             <TextAppearanceIcon color="black" />
         </button>
-        <button class="dy-btn-sm dy-btn-ghost">
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(3)}
+            class:activeButton={active_editor_index == 3}
+        >
+            <TextAppearanceIcon color="orange" />
+        </button>
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(4)}
+            class:activeButton={active_editor_index == 4}
+        >
             <TextAppearanceIcon color="black" />
         </button>
-        <button class="dy-btn-sm dy-btn-ghost">
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(5)}
+            class:activeButton={active_editor_index == 5}
+        >
+            <TextAppearanceIcon color="yellow" />
+        </button>
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(6)}
+            class:activeButton={active_editor_index == 6}
+        >
             <TextAppearanceIcon color="black" />
         </button>
-        <button class="dy-btn-sm dy-btn-ghost">
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(7)}
+            class:activeButton={active_editor_index == 7}
+        >
+            <TextAppearanceIcon color="green" />
+        </button>
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(8)}
+            class:activeButton={active_editor_index == 8}
+        >
             <TextAppearanceIcon color="black" />
         </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
-        </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
-        </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
-        </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
-        </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
-        </button>
-        <button class="dy-btn-sm dy-btn-ghost">
-            <TextAppearanceIcon color="black" />
+        <button
+            class="dy-btn-sm dy-btn-ghost"
+            on:click={() => centerButton(9)}
+            class:activeButton={active_editor_index == 9}
+        >
+            <TextAppearanceIcon color="blue" />
         </button>
     </div>
 
-    <div id="editorsPane" style="border: 2px solid orange;">
-        <!-- Pane for the current editor -->
-        <h1 id="DEBUG1" style="height:300px; border: 2px solid green;">Editor Content</h1>
+    <!-- Pane for the current editor -->
+    <div
+        id="editorsPane"
+        class="dy-w-64 dy-carousel dy-rounded-box"
+        style="border: 2px solid orange;"
+    >
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 0</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 1</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 2</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 3</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 4</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 5</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 6</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 7</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 8</h1>
+        </div>
+
+        <div class="dy-carousel-item" style="width:100%;">
+            <h1 style="height:300px; width:100%;">Editor Content 9</h1>
+        </div>
     </div>
 </div>
 
@@ -122,5 +256,9 @@ The verse on image component.
 
     #editorTabs {
         scrollbar-width: none;
+    }
+
+    #editorTabs button.activeButton {
+        border-bottom: 2px solid black;
     }
 </style>
