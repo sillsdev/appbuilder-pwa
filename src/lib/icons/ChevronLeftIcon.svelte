f633a4fe3f20f14eab13160cb3df36eaa13343b1
jakedcolbert <jake.colbert@gordon.edu>
2023-07-25 12:23:04 -0400

    Updated icon colors

--- src/lib/icons/ChevronLeftIcon.svelte
+++ src/lib/icons/ChevronLeftIcon.svelte
@@ -4,12 +4,11 @@
     // Weight = 400
     // Grade = 0
     // Optical size = 24px
-    export let color = 'black';
+    export let color = 'grey';
     export let size = 24;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
-    ><g transform="rotate(180 12 12)"
-        ><path fill={color} d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" /></g
+<svg fill={color} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
+    ><g transform="rotate(180 12 12)"><path d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" /></g
     ></svg
 >
--- src/lib/icons/ChevronRightIcon.svelte
+++ src/lib/icons/ChevronRightIcon.svelte
@@ -4,10 +4,10 @@
     // Weight = 400
     // Grade = 0
     // Optical size = 24px
-    export let color = 'black';
+    export let color = 'grey';
     export let size = 24;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
-    ><path fill={color} d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" /></svg
+<svg fill={color} xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24"
+    ><path d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6z" /></svg
 >
