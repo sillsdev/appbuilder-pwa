cfe4700e12bd56819d5766ba146f7d9ae4aa1e0b
Chris Hubbard <chris@thehubbards.org>
2023-08-04 09:53:05 -0400

    Use Email & Subject configuration on share (#386)
    * Share annotations use email and subject
    
    * include the title in the file share when falling back

--- src/lib/data/annotation-share.ts
+++ src/lib/data/annotation-share.ts
@@ -3,8 +3,22 @@ import { shareText } from '$lib/data/share';
 
 const SHARE_FILE_NAME = 'annotations.txt';
 
+function title() {
+    let text = '';
+    if (config.mainFeatures['annotation-share-subject']) {
+        text += config.mainFeatures['annotation-share-subject'] + '\n';
+    } else {
+        text += config.name + '\n';
+    }
+    if (config.mainFeatures['annotation-share-email']) {
+        text += config.mainFeatures['annotation-share-email'] + '\n';
+    }
+    return text;
+}
+
 export async function shareAnnotation(annotation: any) {
-    await shareText(config.name, annotation.reference + '\n' + annotation.text, SHARE_FILE_NAME);
+    let text = annotation.reference + '\n' + annotation.text;
+    await shareText(title(), text, SHARE_FILE_NAME);
 }
 
 export async function shareAnnotations(annotations: any) {
@@ -13,5 +27,5 @@ export async function shareAnnotations(annotations: any) {
         '\n\n' +
         annotations.map((item) => `${item.reference}\n${item.text}`).join('\n\n');
 
-    await shareText(config.name, text, SHARE_FILE_NAME, true);
+    await shareText(title(), text, SHARE_FILE_NAME, true);
 }
