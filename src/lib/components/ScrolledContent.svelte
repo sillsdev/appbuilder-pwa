6e5ee3944c1d3baba77cb67ce21a623aa9b06e8d
davidmoore1 <david_moore1@sil.org>
2023-11-16 09:49:28 -0500

    Revert changes to ScrolledContent for settings

--- src/lib/components/ScrolledContent.svelte
+++ src/lib/components/ScrolledContent.svelte
@@ -2,26 +2,13 @@
     import { mainScroll } from '$lib/data/stores';
     import { onMount } from 'svelte';
     let main: HTMLElement;
-    function inViewport() {
-        if (!main) return 0;
 
-        const elH = main.offsetHeight;
-        const H = window.innerHeight;
-        const r = main.getBoundingClientRect();
-        const t = r.top;
-        const b = r.bottom;
-        return [-t, Math.max(0, t > 0 ? Math.min(elH, H - t) : Math.min(b, H))];
-    }
-    export var updated = false;
     export const updateScroll = (() => {
         let updateTimer: NodeJS.Timeout;
         return () => {
             clearTimeout(updateTimer);
             updateTimer = setTimeout(() => {
-                // let viewVal = inViewport();
-                // console.log("ViewVal: " + viewVal);
-                // console.log("View main: %d %d", main.scrollTop, main.clientHeight);
-                // $mainScroll = { top: viewVal[0], height: viewVal[1] };
+                $mainScroll = { top: main.scrollTop, height: main.clientHeight };
             }, 50);
         };
     })();
