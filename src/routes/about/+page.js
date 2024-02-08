78bb0fed94947190a63f691da9c9b03ac40eef98
Chris Hubbard <chris@thehubbards.org>
2024-02-08 14:52:20 -0500

    Work-around fetch in about
    * We should be doing, but it is not working:
      export async function load( { fetch } ) { await
        fetch('/about.partial.html?raw');
      }
    * restore using window.fetch for now
    * update style to use width limited to md breakpoint

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,5 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+    // use window.fetch for now so that the ?raw works.
     const response = await fetch('/about.partial.html?raw');
     const partial = await response.text();
     return { partial };
