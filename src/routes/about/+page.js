3326fba6426a1a0166f182b32da9c9608603aa70
Chris Hubbard <chris@thehubbards.org>
2024-02-09 13:44:45 -0500

    Fix about at BUILD_BASE_PATH (#441)
    * need to use {base} for path to get partial
    * remove `?raw` since I think it was needed when
      we tried to import the partial and now that we are
      loading as resource via fetch it is not needed

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,7 +1,8 @@
+import { base } from '$app/paths';
+
 /** @type {import('./$types').PageLoad} */
-export async function load() {
-    // use window.fetch for now so that the ?raw works.
-    const response = await fetch('/about.partial.html?raw');
+export async function load({ fetch }) {
+    const response = await fetch(`${base}/about.partial.html`);
     const partial = await response.text();
     return { partial };
 }
