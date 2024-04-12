ac15d1667d7833cd9c80c4aa57598feb7160866e
davidmoore1 <david_moore1@sil.org>
2024-04-12 17:03:40 -0400

    Footnotes display on all collections after it displays once (Issue #455)

--- src/lib/components/ScriptureViewSofria.svelte
+++ src/lib/components/ScriptureViewSofria.svelte
@@ -736,7 +736,7 @@ TODO:
                                     addIllustrations(illustrations);
                                 }
                             }
-                            addFooter(document, container, docSet);
+                            addFooter(document, workspace.root, docSet);
                         }
                     }
                 ],
