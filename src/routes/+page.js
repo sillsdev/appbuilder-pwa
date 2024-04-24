4ea5868abb43f6166a3e6a89e8ac04adc885b570
Chris Hubbard <chris@thehubbards.org>
2024-04-24 11:21:10 -0400

    Fix warning about top level awaits going away in Svelte-Kit 2.0

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,10 +1,10 @@
 import { initProskomma } from '$lib/data/scripture';
 
 /** @type {import('./$types').PageLoad} */
-export function load({ url, fetch }) {
+export async function load({ url, fetch }) {
     const ref = url.searchParams.get('ref');
     const audio = url.searchParams.get('audio');
-    const proskomma = initProskomma({ fetch });
+    const proskomma = await initProskomma({ fetch });
 
     return { ref, audio, proskomma };
 }
