49fcd94e0bbe6a17847f8eeb6055eb6d2dbf7587
Garrett Jones <jones.garrett_m@outlook.com>
2024-05-06 16:20:12 -0500

    Add numeralSystem from SAB
    In the native app, numeralSystem is defined at:
        `java/org/sil/app/lib/common/utils/NumeralSystem.java`
    
    Used ChatGPT to convert Java enum to TypeScript.

---
+++ src/lib/scripts/numeralSystem.ts
@@ -0,0 +1,35 @@
+enum NumeralSystem {
+    DEFAULT = 'Default',
+    BENGALI = 'Bengali',
+    DEVANAGARI = 'Devanagari',
+    EASTERN_ARABIC = 'Arabic',
+    GUJARARTI = 'Gujararti',
+    GURMUKHI = 'Gurmukhi',
+    KANNADA = 'Kannada',
+    KHMER = 'Khmer',
+    LAO = 'Lao',
+    LIMBU = 'Limbu',
+    MALAYALAM = 'Malayalam',
+    MONGOLIAN = 'Mongolian',
+    MYANMAR = 'Myanmar',
+    NKO = "N'Ko",
+    ORIYA = 'Oriya',
+    PERSIAN = 'Persian',
+    TAMIL = 'Tamil',
+    TELUGU = 'Telugu',
+    THAI = 'Thai',
+    TIBETAN = 'Tibetan'
+}
+
+namespace NumeralSystem {
+    export function fromString(text: string): NumeralSystem {
+        const system = Object.values(NumeralSystem).find((system) => system === text);
+        return (system as NumeralSystem) || NumeralSystem.DEFAULT;
+    }
+
+    export function getStringList(): string[] {
+        return Object.values(NumeralSystem) as string[];
+    }
+}
+
+export { NumeralSystem };
