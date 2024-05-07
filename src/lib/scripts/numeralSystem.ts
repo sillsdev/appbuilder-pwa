b000ce6b548d4b9e9880748ee577d621f09d5dc7
Garrett Jones <jones.garrett_m@outlook.com>
2024-05-07 13:46:53 -0500

    Add functions format numerals to other systems
    Based on the following Java code in the app-builders repo:
    
    source/common/app-lib-common/src/main/java/org/sil/app/lib/common/utils/NumeralUtils.java

--- src/lib/scripts/numeralSystem.ts
+++ src/lib/scripts/numeralSystem.ts
@@ -1,31 +1,49 @@
-export enum NumeralSystem {
-    DEFAULT = 'Default',
-    BENGALI = 'Bengali',
-    DEVANAGARI = 'Devanagari',
-    EASTERN_ARABIC = 'Arabic',
-    GUJARARTI = 'Gujararti',
-    GURMUKHI = 'Gurmukhi',
-    KANNADA = 'Kannada',
-    KHMER = 'Khmer',
-    LAO = 'Lao',
-    LIMBU = 'Limbu',
-    MALAYALAM = 'Malayalam',
-    MONGOLIAN = 'Mongolian',
-    MYANMAR = 'Myanmar',
-    NKO = "N'Ko",
-    ORIYA = 'Oriya',
-    PERSIAN = 'Persian',
-    TAMIL = 'Tamil',
-    TELUGU = 'Telugu',
-    THAI = 'Thai',
-    TIBETAN = 'Tibetan'
-}
+/**
+ * Represent numbers in an internathional numeral system.
+ */
+export const NumeralSystemData = {
+    DEFAULT: { name: 'Default', zero: '0' },
+    BENGALI: { name: 'Bengali', zero: '\u09E6' },
+    DEVANAGARI: { name: 'Devanagari', zero: '\u0966' },
+    EASTERN_ARABIC: { name: 'Arabic', zero: '\u0660' },
+    GUJARARTI: { name: 'Gujararti', zero: '\u0AE6' },
+    GURMUKHI: { name: 'Gurmukhi', zero: '\u0A66' },
+    KANNADA: { name: 'Kannada', zero: '\u0CE6' },
+    KHMER: { name: 'Khmer', zero: '\u17E0' },
+    LAO: { name: 'Lao', zero: '\u0ED0' },
+    LIMBU: { name: 'Limbu', zero: '\u1946' },
+    MALAYALAM: { name: 'Malayalam', zero: '\u0D66' },
+    MONGOLIAN: { name: 'Mongolian', zero: '\u1810' },
+    MYANMAR: { name: 'Myanmar', zero: '\u1040' },
+    NKO: { name: "N'Ko", zero: '\u07C0' },
+    ORIYA: { name: 'Oriya', zero: '\u0B66' },
+    PERSIAN: { name: 'Persian', zero: '\u06F0' },
+    TAMIL: { name: 'Tamil', zero: '\u0BE6' },
+    TELUGU: { name: 'Telugu', zero: '\u0C66' },
+    THAI: { name: 'Thai', zero: '\u0E50' },
+    TIBETAN: { name: 'Tibetan', zero: '\u0F20' }
+} as const;
+
+export type NumeralSystem = (typeof NumeralSystemData)[keyof typeof NumeralSystemData];
 
 export function fromString(text: string): NumeralSystem {
-    const system = Object.values(NumeralSystem).find((system) => system === text);
-    return (system as NumeralSystem) || NumeralSystem.DEFAULT;
+    const system = Object.values(NumeralSystemData).find((system) => system.name === text);
+    return system || NumeralSystemData.DEFAULT;
 }
 
 export function getStringList(): string[] {
-    return Object.values(NumeralSystem) as string[];
+    return Object.values(NumeralSystemData).map((system) => system.name);
+}
+
+export function formatNumber(system: NumeralSystem, value: string): any {
+    let fmt = '';
+    for (let i = 0; i < value.length; i++) {
+        const digit = Number(value.charAt(i));
+        fmt += formatDigit(system, digit);
+    }
+    return fmt;
+}
+
+function formatDigit(system: NumeralSystem, value: number) {
+    return String.fromCharCode(system.zero.charCodeAt(0) + value);
 }
