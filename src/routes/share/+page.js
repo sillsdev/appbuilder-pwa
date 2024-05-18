c15120fc636ecb32a986407a867ddb05c11b0ee9
Chris Hubbard <chris@thehubbards.org>
2024-05-18 11:10:19 -0400

    Fix lint & remove log

--- src/routes/share/+page.js
+++ src/routes/share/+page.js
@@ -3,13 +3,11 @@ import { base } from '$app/paths';
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
     let languages = [];
-    const response = await fetch(`${base}/badges/languages.json`)
+    const response = await fetch(`${base}/badges/languages.json`);
     const result = await response.json();
     if (!result.error) {
-        console.log(result, JSON.stringify(result));
         languages = result;
     }
-
 
     return { languages };
 }
