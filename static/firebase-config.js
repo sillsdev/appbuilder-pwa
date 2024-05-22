5a78a151532d8e0e59e9fbcf5c70ed2c9f01a2d1
Aidan Scott <aidanpscott@gmail.com>
2024-05-22 10:27:33 -0400

    A bunch of files got removed, so I had to rebuild PWA data files

---
+++ static/firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyANPntOi_siWq_EuCQolND3zIXSqPe8pOQ",
+  authDomain: "web-gospels.firebaseapp.com",
+  databaseURL: "https://web-gospels-default-rtdb.firebaseio.com",
+  projectId: "web-gospels",
+  storageBucket: "web-gospels.appspot.com",
+  messagingSenderId: "318919075501",
+  appId: "1:318919075501:web:8b1f900eac3b500edc8824",
+  measurementId: "G-0JJYW5X47N"
+};
