1496610c1e791f07b61762899c7407dbfdbbbfef
Aidan Scott <aidanpscott@gmail.com>
2024-05-22 10:28:14 -0400

    the +page.js file for the quiz page now fetches quiz.json

--- src/routes/quiz/[docset]/[collection]/[id]/+page.js
+++ src/routes/quiz/[docset]/[collection]/[id]/+page.js
@@ -2,5 +2,19 @@
 export async function load({ params }) {
     const id = Number(params.id);
     const collection = params.collection;
-    return {};
+
+    try {
+        const response = await fetch('quiz.json');
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch quiz.json file');
+        }
+
+        const quizData = await response.json()
+        return { quizData };
+    }
+    catch (error) {
+        console.error("Error fetching quiz.json file:', error")
+        return {};
+    }
 }
