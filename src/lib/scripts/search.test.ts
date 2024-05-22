bb53467d546fe0ef79f78b86a1db778521d2a330
Garrett Jones <jones.garrett_m@outlook.com>
2024-05-22 18:23:50 -0500

    Parse GraphQL query response into search results

--- src/lib/scripts/search.test.ts
+++ src/lib/scripts/search.test.ts
@@ -1,6 +1,13 @@
+import { readFileSync } from 'fs';
 import { test, describe, expect } from 'vitest';
 
-import { searchParams, tokenize } from './search';
+import {
+    parseResponse,
+    referenceFromScopes,
+    searchParams,
+    tokenize,
+    type GraphQLResponse
+} from './search';
 
 describe('searchParams', () => {
     describe('whole words', () => {
@@ -109,14 +116,116 @@ describe('searchParams', () => {
             expect(result).toBe('withMatchingChars: ["Laz\\\\-.*"]');
         });
     });
+});
 
-    describe('tokenize', () => {
-        test('splits on whitespace', () => {
-            expect(tokenize('  some\n thing\tgreat ')).toEqual(['some', 'thing', 'great']);
-        });
-
-        test('Retuns empty array if only whitespace', () => {
-            expect(tokenize('  \n \t ')).toEqual([]);
-        });
+describe('tokenize', () => {
+    test('splits on whitespace', () => {
+        expect(tokenize('  some\n thing\tgreat ')).toEqual(['some', 'thing', 'great']);
     });
+
+    test('Retuns empty array if only whitespace', () => {
+        expect(tokenize('  \n \t ')).toEqual([]);
+    });
+});
+
+describe('referenceFromScopes', () => {
+    test('chapter then verses', () => {
+        const scopes = ['chapter/11', 'verses/1'];
+        expect(referenceFromScopes(scopes)).toBe('11:1');
+    });
+});
+
+describe('parseResponse', () => {
+    const response1 = JSON.parse(
+        readFileSync('test_data/sampleSearchResults/martha_said.json').toString()
+    ) as GraphQLResponse;
+
+    const searchResults1 = [
+        {
+            reference: {
+                bookCode: 'JHN',
+                chapter: '11',
+                verses: '21'
+            },
+            chunks: [
+                {
+                    content: 'Therefore ',
+                    matchesQuery: false
+                },
+                {
+                    content: 'Martha said',
+                    matchesQuery: true
+                },
+                {
+                    content:
+                        ' to Jesus, “Lord, if you would have been here, my brother wouldn’t have died. ',
+                    matchesQuery: false
+                }
+            ]
+        },
+        {
+            reference: {
+                bookCode: 'JHN',
+                chapter: '11',
+                verses: '24'
+            },
+            chunks: [
+                {
+                    content: 'Martha said',
+                    matchesQuery: true
+                },
+                {
+                    content:
+                        ' to him, “I know that he will rise again in the resurrection at the last day.”',
+                    matchesQuery: false
+                }
+            ]
+        }
+    ];
+
+    /*
+     * In this test response, John 11:35 has been intentionally broken into two blocks.
+     * This situation is more likely in passages such as the Psalms.
+     * The search algorithm should be able to discover text that spans multiple blocks within the same verse.
+     */
+    const response2 = JSON.parse(
+        readFileSync('test_data/sampleSearchResults/martha_said.json').toString()
+    ) as GraphQLResponse;
+
+    const searchResults2 = [
+        {
+            reference: {
+                bookCode: 'JHN',
+                chapter: '11',
+                verses: '35'
+            },
+            chunks: [
+                {
+                    content: 'Jesus wept',
+                    matchesQuery: true
+                },
+                {
+                    content: '.',
+                    matchesQuery: false
+                }
+            ]
+        }
+    ];
+
+    test('finds "Martha said"', () => {
+        expect(parseResponse(response1, 'Martha said')).toEqual(searchResults1);
+    });
+
+    test('ignores leading and trailing whitespace', () => {
+        expect(parseResponse(response1, '  Martha said   ')).toEqual(searchResults1);
+    });
+
+    /*
+     * In response2, John 11:35 has been intentionally broken into two blocks.
+     * This situation is more likely in passages such as the Psalms.
+     * The search algorithm should be able to discover text that spans multiple blocks within the same verse.
+     */
+    // test('finds text accross multiple blocks', () => {
+    //     expect(parseResponse(response2, 'Jesus wept')).toEqual(searchResults2);
+    // });
 });
