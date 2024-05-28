62f4677336e3de46f3e04e6aef61f1581fe616f9
Aidan Scott <aidanpscott@gmail.com>
2024-05-28 09:37:21 -0400

    quiz squash #1
    trying to center answer buttons and also preparing to implement audio
    files
    
    Quiz questions programmed to play questions based on correctness of
    answer.
    
    Centered the answer block correctly for quizzes
    
    Deleted old quiz JSON file and got quiz page to detect correct from
    incorrect answers.
    
    created 2 separate quiz answer blocks for text and images, then
    separated them in if statements
    
    Removed some redundancies in the quiz page code
    
    Everytime you get a quiz question right, a point is added to your score
    
    I randomized the order in which quiz answers are displayed.
    
    Removed some excess comments
    
    Quiz buttons now get highlighted green when answered correctly
    
    When a wrong quiz answer is selected, it's highlighted in red.
    
    Clicking a quiz answer now colors the answer box red or green rather
    than what's behind it.
    
    Set a timer after a quiz answer is guessed to give the user some time to
    observe the result of their answer.
    
    After a quiz, you now get a score.
    
    After a quiz, you now get a message if you getting a passing score.
    
    Got rid of unused imports from the quiz Svelte page
    
    removed unneeded console logs from the quiz svelte page
    
    Divided elements of the score for quizzes in their own div's
    
    put the elements of quiz score in flex grids with margins
    
    I changed the quiz page code so the navbar is outside the if statement
    and doesn't get created when switching between score and the quiz itself
    
    Finished the quiz score page styling
    
    The clicked quiz answer text is now white, and images now display images
    
    Started implementing next arrow per quiz question.
    
    Paired quiz answers when in image format
    
    Put a quiz label on the navbar
    
    Corrected the color change for quiz answers that are images to the
    correct ones
    
    Added a display: block to the class of quiz answer images
    
    Updated the quiz fork so shuffling answers works and correct answer is
    highlighted when incorrect quiz answers are selected
    
    The next button for quizzes is now centered properly
    
    For quizzes, fixed width and padding of image answers a bit. Got the
    image highlighting working.
    
    Completely fixed highlighting for quiz image questions.
    
    Fixed an issue for quiz questions where question images wasn't being
    centered.
    
    Removed pairing from quiz page so columns is now handled by the quiz
    itself
    
    gave question-images a class name of quiz-question-image
    
    For the quizzes, I made the next button an identifier
    
    Quiz image answers are now columned
    
    Quiz answers now have flexible sizes for lg, md, and small screens
    
    For quizzes, score messages "You Pass" and "Oh Dear" are now centered
    properly.
    
    Removed some unneeded comments.

--- src/routes/quiz/[collection]/[id]/+page.svelte
+++ src/routes/quiz/[collection]/[id]/+page.svelte
@@ -1,68 +1,205 @@
 <script>
     import Navbar from '$lib/components/Navbar.svelte';
-    import { t } from '$lib/data/stores';
+    import { refs } from '$lib/data/stores';
     import config from '$lib/data/config';
     import { base } from '$app/paths';
-    import { page } from '$app/stores';
-
+    import { onMount } from 'svelte';
     /** @type {import('./$types').PageData} */
     export let data;
 
-    console.log(JSON.stringify(data, null, 4));
-
+    let quiz = data.quiz;
+    let shuffledAnswers = [];
     let score = 0;
     let questionNum = 0;
+    let clicked = false;
+    let displayCorrect = false;
+    function playSound(path) {
+        const audio = new Audio();
+        audio.src = path;
+        audio.play();
+    }
+    function getImageSource(image) {
+        let source = `${base}/illustrations/${$refs.collection}-${quiz.id}-${image}`;
+        return source;
+    }
+    function shuffleAnswers(answerArray) {
+        let currentIndex = answerArray.length,
+            randomIndex;
+        while (currentIndex != 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+            [answerArray[currentIndex], answerArray[randomIndex]] = [
+                answerArray[randomIndex],
+                answerArray[currentIndex]
+            ];
+        }
+        return answerArray;
+    }
+    function handleQuestionChange() {
+        if (questionNum == quiz.questions.length) {
+            shuffledAnswers = [];
+        } else {
+            for (const answer of quiz.questions[questionNum].answers) {
+                answer.clicked = false;
+            }
+            shuffledAnswers = shuffleAnswers(quiz.questions[questionNum].answers);
+        }
+    }
+    function onNextQuestion() {
+        questionNum++;
+        clicked = false;
+        displayCorrect = false;
+        handleQuestionChange();
+    }
+    function onQuestionAnswered(answer) {
+        const audioPath = answer.correct
+            ? `${base}/assets/quiz-right-answer.mp3`
+            : `${base}/assets/quiz-wrong-answer.mp3`;
+        playSound(audioPath);
+        if (answer.correct) {
+            score++;
+        }
+        setTimeout(() => {
+            answer.clicked = true;
+            clicked = true;
+            if (answer.correct) {
+                displayCorrect = true;
+            } else {
+                setTimeout(() => {
+                    displayCorrect = true;
+                }, 1000);
+            }
+        }, 1000);
+    }
+    onMount(() => {
+        questionNum = 0;
+        handleQuestionChange();
+    });
 </script>
 
 <div class="grid grid-rows-[auto,1fr] h-screen">
     <div class="navbar">
         <Navbar>
-            <!-- <div slot="left-buttons" /> -->
-            <label for="dropdown" slot="books">
-                <!-- CHECK ABOVE!!!! -->
+            <label for="Quiz" slot="center">
                 <div class="btn btn-rectangel normal-case text-xl">{'Quiz'}</div>
-                <!-- CHECK ABOVE!!!! -->
             </label>
-            <!-- <div slot="right-buttons" /> -->
         </Navbar>
     </div>
-    <body class="quiz">
-        <div id="content">
-            <div class="quiz-question-number"></div>
-            <div class="quiz-question-block">
-                <div class="quiz-question">
-                    {data.quiz.questions[questionNum].text}
-                    {#if data.quiz.questions[questionNum].image}
-                        {data.quiz.questions[questionNum].image}
-                    {/if}
+    {#if questionNum == quiz.questions.length}
+        <div class="score flex justify-center">
+            <div id="content" class="text-center">
+                <div class="quiz-score-before">You scored</div>
+                <div class="quiz-score-block">
+                    <span class="quiz-score">{score}</span>
                 </div>
-            </div>
-            <table class="grid w-[32rem] justify-center mt-10">
-                {#if data.quiz.questions[questionNum].answers}
-                    <!-- If statement checks if questions have all been answered. -->
-                    {#each data.quiz.questions[questionNum].answers as answer}
-                        <tr>
-                            <td>
-                                <button
-                                    class="w-full"
-                                    on:click={() => {
-                                        questionNum++;
-                                    }}
-                                >
-                                    <div class="quiz-answer">
-                                        {#if answer.text}
-                                            {answer.text}
-                                        {/if}
-                                        {#if answer.image}
-                                            {answer.image}
-                                        {/if}
-                                    </div>
-                                </button>
-                            </td>
-                        </tr>
-                    {/each}
+                <div class="quiz-score-after">
+                    out of {questionNum} questions.
+                </div>
+                {#if score == quiz.passScore || score > quiz.passScore}
+                    <div class="mt-6 text-3xl font-bold text-green-500">You pass!</div>
+                {:else if score < quiz.passScore}
+                    <div class="mt-6 text-3xl font-bold text-red-500">Oh, dear!</div>
                 {/if}
-            </table>
+            </div>
         </div>
-    </body>
+    {:else}
+        <body class="quiz">
+            <div id="content">
+                <div class="quiz-question-number">{questionNum + 1}</div>
+                {#if quiz.questions[questionNum].answers}
+                    {#if quiz.questions[questionNum].answers.some((answer) => answer.text)}
+                        <div class="quiz-question-block">
+                            <div class="quiz-question">
+                                {quiz.questions[questionNum].text}
+                                {#if quiz.questions[questionNum].image}
+                                    <div class="flex justify-center">
+                                        <img
+                                            class="quiz-question-image"
+                                            src={getImageSource(quiz.questions[questionNum].image)}
+                                        />
+                                    </div>
+                                {/if}
+                            </div>
+                            <div class="flex quiz-answer-block justify-center">
+                                <table class="mt-10">
+                                    {#each shuffledAnswers as answer}
+                                        <tr>
+                                            <td>
+                                                <button
+                                                    class="flex-initial w-64 md:w-[22rem] lg:w-[32rem] mt-2 gap-8}"
+                                                    on:click={() => {
+                                                        onQuestionAnswered(answer);
+                                                    }}
+                                                >
+                                                    <div
+                                                        class="quiz-answer flex justify-center"
+                                                        style="{clicked &&
+                                                        answer.clicked &&
+                                                        !answer.correct
+                                                            ? 'color: rgb(255, 255, 255); background-color: rgb(128,0,0)'
+                                                            : ''} {clicked &&
+                                                        answer.correct &&
+                                                        (answer.clicked || displayCorrect)
+                                                            ? 'color: rgb(255, 255, 255); background-color: rgb(0,128,0)'
+                                                            : ''}"
+                                                    >
+                                                        {answer.text}
+                                                    </div>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    {/each}
+                                </table>
+                            </div>
+                        </div>
+                    {/if}
+                    {#if quiz.questions[questionNum].answers.some((answer) => answer.image)}
+                        <div class="quiz-question-block">
+                            <div class="quiz-question">
+                                {quiz.questions[questionNum].text}
+                            </div>
+                            <div class="flex justify-center flex-wrap gap-4">
+                                <div class="grid grid-cols-2 gap-4">
+                                    {#each shuffledAnswers as answer}
+                                        <div class="w-full flex justify-center">
+                                            <img
+                                                class="cursor-pointer w-32 h-32 rounded-md"
+                                                src={getImageSource(answer.image)}
+                                                alt={answer.text}
+                                                style="{clicked && answer.clicked && !answer.correct
+                                                    ? 'color: rgb(255, 255, 255); background-color: rgb(128,0,0)'
+                                                    : ''} {clicked &&
+                                                answer.correct &&
+                                                (answer.clicked || displayCorrect)
+                                                    ? 'color: rgb(255, 255, 255); background-color: rgb(0,128,0)'
+                                                    : ''}"
+                                                on:click={() => {
+                                                    onQuestionAnswered(answer);
+                                                }}
+                                            />
+                                        </div>
+                                    {/each}
+                                </div>
+                            </div>
+                        </div>
+                    {/if}
+                {/if}
+                {#if displayCorrect}
+                    <div
+                        class="quiz-next-button arrow-ltr flex justify-center items-center"
+                        style="cursor: pointer;"
+                    >
+                        <button
+                            class=""
+                            on:click={() => {
+                                onNextQuestion();
+                            }}
+                        >
+                            <div>{'quiz-next-button'}</div>
+                        </button>
+                    </div>
+                {/if}
+            </div>
+        </body>
+    {/if}
 </div>
