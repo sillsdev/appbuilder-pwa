7437181c12368c5e179582588ab1480c9ad4f355
Aidan Scott <aidanpscott@gmail.com>
2024-06-04 09:22:31 -0400

    Updated the quiz fork so shuffling answers works and correct answer is highlighted when incorrect quiz answers are selected

--- src/routes/quiz/[collection]/[id]/+page.svelte
+++ src/routes/quiz/[collection]/[id]/+page.svelte
@@ -1,22 +1,28 @@
 <script>
     import Navbar from '$lib/components/Navbar.svelte';
+    import { refs } from '$lib/data/stores';
     import config from '$lib/data/config';
     import { base } from '$app/paths';
-
+    import { onMount } from 'svelte';
     /** @type {import('./$types').PageData} */
     export let data;
-
     // console.log(JSON.stringify(data, null, 4));
-
+    let quiz = data.quiz;
+    let shuffledAnswers = [];
+    let pairs = [];
     let score = 0;
     let questionNum = 0;
-
+    let clicked = false;
+    let displayCorrect = false;
     function playSound(path) {
         const audio = new Audio();
         audio.src = path;
         audio.play();
     }
-
+    function getImageSource(image) {
+        let source = `${base}/illustrations/${$refs.collection}-${quiz.id}-${image}`;
+        return source;
+    }
     function pairAnswers(answers) {
         const pairs = [];
         for (let i = 0; i < answers.length; i += 2) {
@@ -24,23 +30,61 @@
         }
         return pairs;
     }
-
     function shuffleAnswers(answerArray) {
         let currentIndex = answerArray.length,
             randomIndex;
-
         while (currentIndex != 0) {
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex--;
-
             [answerArray[currentIndex], answerArray[randomIndex]] = [
                 answerArray[randomIndex],
                 answerArray[currentIndex]
             ];
         }
-
         return answerArray;
     }
+    function handleQuestionChange() {
+        if (questionNum == quiz.questions.length) {
+            shuffledAnswers = [];
+            pairs = [];
+        } else {
+            for (const answer of quiz.questions[questionNum].answers) {
+                answer.clicked = false;
+            }
+            shuffledAnswers = shuffleAnswers(quiz.questions[questionNum].answers);
+            pairs = pairAnswers(shuffledAnswers);
+        }
+    }
+    function onNextQuestion() {
+        questionNum++;
+        clicked = false;
+        displayCorrect = false;
+        handleQuestionChange();
+    }
+    function onQuestionAnswered(answer) {
+        const audioPath = answer.correct
+            ? `${base}/assets/quiz-right-answer.mp3`
+            : `${base}/assets/quiz-wrong-answer.mp3`;
+        playSound(audioPath);
+        if (answer.correct) {
+            score++;
+        }
+        setTimeout(() => {
+            answer.clicked = true;
+            clicked = true;
+            if (answer.correct) {
+                displayCorrect = true;
+            } else {
+                setTimeout(() => {
+                    displayCorrect = true;
+                }, 1000);
+            }
+        }, 1000);
+    }
+    onMount(() => {
+        questionNum = 0;
+        handleQuestionChange();
+    });
 </script>
 
 <div class="grid grid-rows-[auto,1fr] h-screen">
@@ -51,7 +95,7 @@
             </label>
         </Navbar>
     </div>
-    {#if questionNum == data.quiz.questions.length}
+    {#if questionNum == quiz.questions.length}
         <body class="score">
             <div id="content">
                 <div class="quiz-score-before">You scored</div>
@@ -61,9 +105,9 @@
                 <div class="quiz-score-after">
                     out of {questionNum} questions.
                 </div>
-                {#if score == data.quiz.passScore || score > data.quiz.passScore}
+                {#if score == quiz.passScore || score > quiz.passScore}
                     <div class="flex justify-center mt-50">You pass!</div>
-                {:else if score < data.quiz.passScore}
+                {:else if score < quiz.passScore}
                     <div class="flex justify-center mt-50">Oh, dear!</div>
                 {/if}
             </div>
@@ -72,41 +116,39 @@
         <body class="quiz">
             <div id="content">
                 <div class="quiz-question-number">{questionNum + 1}</div>
-                {#if data.quiz.questions[questionNum].answers}
-                    {#if data.quiz.questions[questionNum].answers.some((answer) => answer.text)}
+                {#if quiz.questions[questionNum].answers}
+                    {#if quiz.questions[questionNum].answers.some((answer) => answer.text)}
                         <div class="quiz-question-block">
                             <div class="quiz-question">
-                                {data.quiz.questions[questionNum].text}
-                                {#if data.quiz.questions[questionNum].image}
-                                    {data.quiz.questions[questionNum].image}
+                                {quiz.questions[questionNum].text}
+                                {#if quiz.questions[questionNum].image}
+                                    <img
+                                        class="display: block; flex-initial w-[32rem] ml-2 gap-8"
+                                        src={getImageSource(quiz.questions[questionNum].image)}
+                                        alt={quiz.questions[questionNum].text}
+                                    />
                                 {/if}
                             </div>
                             <div class="flex quiz-answer-block justify-center">
                                 <table class="mt-10">
-                                    {#each shuffleAnswers(data.quiz.questions[questionNum].answers) as answer}
+                                    {#each shuffledAnswers as answer}
                                         <tr>
                                             <td>
                                                 <button
                                                     class="flex-initial w-[32rem] mt-2 gap-8}"
                                                     on:click={() => {
-                                                        const audioPath = answer.correct
-                                                            ? `${base}/clips/quiz-right-answer.mp3`
-                                                            : `${base}/clips/quiz-wrong-answer.mp3`;
-                                                        playSound(audioPath);
-                                                        if (answer.correct) {
-                                                            score++;
-                                                        }
-                                                        setTimeout(() => {
-                                                            questionNum++;
-                                                        }, 1000);
-                                                        answer.clicked = true;
+                                                        onQuestionAnswered(answer);
                                                     }}
                                                 >
                                                     <div
                                                         class="quiz-answer flex justify-center items-center"
-                                                        style="{answer.clicked && !answer.correct
+                                                        style="{clicked &&
+                                                        answer.clicked &&
+                                                        !answer.correct
                                                             ? 'color: rgb(255, 255, 255); background-color: rgb(128,0,0)'
-                                                            : ''} {answer.clicked && answer.correct
+                                                            : ''} {clicked &&
+                                                        answer.correct &&
+                                                        (answer.clicked || displayCorrect)
                                                             ? 'color: rgb(255, 255, 255); background-color: rgb(0,128,0)'
                                                             : ''}"
                                                     >
@@ -120,41 +162,34 @@
                             </div>
                         </div>
                     {/if}
-                    {#if data.quiz.questions[questionNum].answers.some((answer) => answer.image)}
+                    {#if quiz.questions[questionNum].answers.some((answer) => answer.image)}
                         <div class="quiz-question-block">
                             <div class="quiz-question">
-                                {data.quiz.questions[questionNum].text}
-                                {#if data.quiz.questions[questionNum].image}
-                                    {data.quiz.questions[questionNum].image}
+                                {quiz.questions[questionNum].text}
+                                {#if quiz.questions[questionNum].image}
+                                    {quiz.questions[questionNum].image}
                                 {/if}
                             </div>
                             <div class="flex quiz-answer-block justify-center">
                                 <table class="mt-10">
-                                    {#each pairAnswers(shuffleAnswers(data.quiz.questions[questionNum].answers)) as [answer1, answer2]}
+                                    {#each pairs as [answer1, answer2]}
                                         <tr>
                                             <td width="50%" style="padding:2%;">
                                                 <img
                                                     class="display: block; flex-initial w-[32rem] ml-2 gap-8"
-                                                    src={answer1.image}
+                                                    src={getImageSource(answer1.image)}
                                                     alt={answer1.text}
-                                                    style="cursor:pointer; {answer1.clicked &&
+                                                    style="cursor:pointer; {clicked &&
+                                                    answer1.clicked &&
                                                     !answer1.correct
                                                         ? 'background-color: red;'
-                                                        : ''} {answer1.clicked && answer1.correct
+                                                        : ''} {clicked &&
+                                                    answer1.clicked &&
+                                                    answer1.correct
                                                         ? 'background-color: green;'
                                                         : ''}"
                                                     on:click={() => {
-                                                        const audioPath = answer1.correct
-                                                            ? `${base}/clips/quiz-right-answer.mp3`
-                                                            : `${base}/clips/quiz-wrong-answer.mp3`;
-                                                        playSound(audioPath);
-                                                        if (answer1.correct) {
-                                                            score++;
-                                                        }
-                                                        setTimeout(() => {
-                                                            questionNum++;
-                                                        }, 1000);
-                                                        answer1.clicked = true;
+                                                        onQuestionAnswered(answer1);
                                                     }}
                                                 />
                                             </td>
@@ -162,7 +197,7 @@
                                                 <td width="50%" style="padding:2%;">
                                                     <img
                                                         class="display: block; flex-initial w-[32rem] ml-2 gap-8"
-                                                        src={answer2.image}
+                                                        src={getImageSource(answer2.image)}
                                                         alt={answer2.text}
                                                         style="cursor:pointer; {answer2.clicked &&
                                                         !answer2.correct
@@ -172,17 +207,7 @@
                                                             ? 'background-color: rgb(0,128,0);'
                                                             : ''}"
                                                         on:click={() => {
-                                                            const audioPath = answer2.correct
-                                                                ? `${base}/clips/quiz-right-answer.mp3`
-                                                                : `${base}/clips/quiz-wrong-answer.mp3`;
-                                                            playSound(audioPath);
-                                                            if (answer2.correct) {
-                                                                score++;
-                                                            }
-                                                            setTimeout(() => {
-                                                                questionNum++;
-                                                            }, 1000);
-                                                            answer2.clicked = true;
+                                                            onQuestionAnswered(answer2);
                                                         }}
                                                     />
                                                 </td>
@@ -194,9 +219,17 @@
                         </div>
                     {/if}
                 {/if}
-                <!--  Quiz-next-button doesn't yet exist in the sab-app.css -->
-                {#if data.quiz.questions[questionNum].answers.clicked}
-                    <div class="quiz-next-button arrow-ltr" style="cursor: pointer;"></div>
+                {#if displayCorrect}
+                    <div class="quiz-next-button arrow-ltr" style="cursor: pointer;">
+                        <button
+                            class="flex-initial w-[32rem] mt-2 gap-8}"
+                            on:click={() => {
+                                onNextQuestion();
+                            }}
+                        >
+                            <div>Next</div>
+                        </button>
+                    </div>
                 {/if}
             </div>
         </body>
