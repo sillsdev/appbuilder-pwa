dfae70f6e4a56adf4532589a0677c298875fbc99
Garrett Jones <jones.garrett_m@outlook.com>
2024-06-08 00:04:30 -0500

    Create regex utils for search

--- src/lib/scripts/search/search.test.ts
+++ src/lib/scripts/search/search.test.ts
@@ -1,5 +1,10 @@
 import { describe, expect, test } from 'vitest';
-import { SearchQueryBase, SearchInterface, type SearchCandidate } from './application';
+import {
+    SearchQueryBase,
+    SearchInterface,
+    type SearchCandidate,
+    type SearchOptions
+} from './application';
 import {
     ProksommaSearchInterface,
     gqlSearchHelpers,
@@ -9,7 +14,6 @@ import {
 } from './adapters';
 import { SABProskomma } from '$lib/sab-proskomma';
 import type { Reference } from './entities';
-import { BufferedReader } from './utils';
 import { thaw } from '../thaw';
 import { readFileSync } from 'fs';
 
@@ -81,13 +85,13 @@ class TestVerseProvider extends SearchInterface.VerseProvider {
 }
 
 class EmptySearchQuery extends SearchQueryBase {
-    constructor(searchPhrase: string, options = { wholeWords: false }) {
+    constructor(searchPhrase: string, options = {}) {
         super(searchPhrase, new EmptyVerseProvider(searchPhrase), options);
     }
 }
 
 class TestSearchQuery extends SearchQueryBase {
-    constructor(searchPhrase: string, options = { wholeWords: false }) {
+    constructor(searchPhrase: string, options: SearchOptions = {}) {
         super(searchPhrase, new TestVerseProvider(searchPhrase), options);
     }
 }
@@ -612,69 +616,4 @@ describe('ProskommaVerseProvider', () => {
 test('chapterVerseFromScopes', () => {
     const scopes = ['chapter/11', 'verses/1'];
     expect(gqlSearchHelpers.chapterVerseFromScopes(scopes)).toBe('11:1');
-});
-
-class TestBufferedInput {
-    i = 0;
-    input = [[1, 2, 3, 4], [5, 6], [7], [], [], [8], [9, 10, 11, 12, 13], [14, 15]];
-
-    async read(limit: number): Promise<number[]> {
-        const data = this.input[this.i];
-        this.i++;
-        return data;
-    }
-
-    done() {
-        return this.i === this.input.length;
-    }
-}
-
-describe('BufferedReader', () => {
-    test('reads all by default', async () => {
-        const input = new TestBufferedInput();
-        const reader = new BufferedReader(input);
-        const data = await reader.read();
-        expect(data.length).toBe(15);
-    });
-
-    test('passes limit to inner read function', async () => {
-        let hasRead = false;
-
-        function read(limit: number): Promise<number[]> {
-            hasRead = true;
-            expect(limit).toBe(5);
-            return Promise.resolve([]);
-        }
-
-        const reader = new BufferedReader({
-            read,
-            done: () => hasRead
-        });
-        await reader.read(5);
-    });
-
-    describe('Read six at a time', async () => {
-        const input = new TestBufferedInput();
-        const reader = new BufferedReader(input);
-        const list1 = await reader.read(6);
-        const list2 = await reader.read(6);
-        const list3 = await reader.read(6);
-        const list4 = await reader.read(6);
-
-        test('First list has 6 items', () => {
-            expect(list1.length).toBe(6);
-        });
-
-        test('Second list has 6 items', () => {
-            expect(list2.length).toBe(6);
-        });
-
-        test('Third list has 3 items', () => {
-            expect(list3.length).toBe(3);
-        });
-
-        test('Last list has no items', () => {
-            expect(list4.length).toBe(0);
-        });
-    });
 });
