19320c95844955779d3038b9f8c3e0db771d44c8
Chris Hubbard <chris@thehubbards.org>
2024-06-18 10:47:55 -0400

    Disable fetching badge information when not needed
    * We only need the badges/languages.json if we are sharing an
      apple app link.

--- src/routes/share/+page.js
+++ src/routes/share/+page.js
@@ -1,12 +1,15 @@
 import { base } from '$app/paths';
+import config from '$lib/data/config';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
     let languages = [];
-    const response = await fetch(`${base}/badges/languages.json`);
-    const result = await response.json();
-    if (!result.error) {
-        languages = result;
+    if (config.mainFeatures['share-apple-app-link']) {
+        const response = await fetch(`${base}/badges/languages.json`);
+        const result = await response.json();
+        if (!result.error) {
+            languages = result;
+        }
     }
 
     return { languages };
