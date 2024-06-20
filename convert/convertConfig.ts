5d5c9e196145e474924b112eaf868c85144370fa
josephshin22 <shinjj22@gcc.edu>
2024-06-20 13:00:25 -0400

    Add version tag

--- convert/convertConfig.ts
+++ convert/convertConfig.ts
@@ -302,11 +302,11 @@ function parseStyles(stylesTag: Element, verbose: number) {
                     if (verbose)
                         console.log(
                             'Parsing ' +
-                                propName +
-                                ' = ' +
-                                propValue +
-                                ' -> ' +
-                                properties[propName]
+                            propName +
+                            ' = ' +
+                            propValue +
+                            ' -> ' +
+                            properties[propName]
                         );
                 } else {
                     //Not a sp value
@@ -619,8 +619,8 @@ function convertConfig(dataDir: string, verbose: number) {
             const fontChoiceTag = book.querySelector('font-choice');
             const fonts = fontChoiceTag
                 ? Array.from(fontChoiceTag.getElementsByTagName('font-choice-family'))
-                      .filter((x) => fontFamilies.includes(x.innerHTML))
-                      .map((x) => x.innerHTML)
+                    .filter((x) => fontFamilies.includes(x.innerHTML))
+                    .map((x) => x.innerHTML)
                 : [];
             const bkAdditionalNames = book.querySelector('additional-names');
             const additionalNames = bkAdditionalNames
@@ -668,8 +668,8 @@ function convertConfig(dataDir: string, verbose: number) {
         if (verbose >= 3) console.log(`.... fontChoice: `, JSON.stringify(fontChoiceTag));
         const fonts = fontChoiceTag
             ? Array.from(fontChoiceTag.getElementsByTagName('font-choice-family'))
-                  .filter((x) => fontFamilies.includes(x.innerHTML))
-                  .map((x) => x.innerHTML)
+                .filter((x) => fontFamilies.includes(x.innerHTML))
+                .map((x) => x.innerHTML)
             : [];
 
         const writingSystem = tag.getElementsByTagName('writing-system')[0];
@@ -770,8 +770,7 @@ function convertConfig(dataDir: string, verbose: number) {
         }
         if (verbose)
             console.log(
-                `Converted ${
-                    Object.keys(data.translationMappings.mappings).length
+                `Converted ${Object.keys(data.translationMappings.mappings).length
                 } translation mappings`
             );
     }
@@ -844,12 +843,12 @@ function convertConfig(dataDir: string, verbose: number) {
                 placementTag == undefined
                     ? undefined
                     : {
-                          pos: placementTag.attributes.getNamedItem('pos')!.value,
-                          ref: placementTag.attributes.getNamedItem('ref')!.value.split('|')[1],
-                          collection: placementTag.attributes
-                              .getNamedItem('ref')!
-                              .value.split('|')[0]
-                      };
+                        pos: placementTag.attributes.getNamedItem('pos')!.value,
+                        ref: placementTag.attributes.getNamedItem('ref')!.value.split('|')[1],
+                        collection: placementTag.attributes
+                            .getNamedItem('ref')!
+                            .value.split('|')[0]
+                    };
             const tagWidth = tag.attributes.getNamedItem('width')
                 ? parseInt(tag.attributes.getNamedItem('width')!.value)
                 : 0;
@@ -905,17 +904,17 @@ function convertConfig(dataDir: string, verbose: number) {
                             placementTag == undefined
                                 ? undefined
                                 : {
-                                      pos: placementTag.attributes.getNamedItem('pos')!.value,
-                                      ref: placementTag.attributes
-                                          .getNamedItem('ref')!
-                                          .value.split('|')[1],
-                                      caption: placementTag.attributes.getNamedItem('caption')
-                                          ? placementTag.attributes.getNamedItem('caption')!.value
-                                          : '',
-                                      collection: placementTag.attributes
-                                          .getNamedItem('ref')!
-                                          .value.split('|')[0]
-                                  };
+                                    pos: placementTag.attributes.getNamedItem('pos')!.value,
+                                    ref: placementTag.attributes
+                                        .getNamedItem('ref')!
+                                        .value.split('|')[1],
+                                    caption: placementTag.attributes.getNamedItem('caption')
+                                        ? placementTag.attributes.getNamedItem('caption')!.value
+                                        : '',
+                                    collection: placementTag.attributes
+                                        .getNamedItem('ref')!
+                                        .value.split('|')[0]
+                                };
                         data.illustrations.push({
                             filename: filename,
                             width: imageWidth,
@@ -952,8 +951,8 @@ function convertConfig(dataDir: string, verbose: number) {
             const layoutCollections =
                 layoutCollectionElements.length > 0
                     ? Array.from(layoutCollectionElements).map((element) => {
-                          return element.attributes.getNamedItem('id')!.value;
-                      })
+                        return element.attributes.getNamedItem('id')!.value;
+                    })
                     : [data.bookCollections[0].id];
 
             data.layouts.push({
