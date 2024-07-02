2556eb4fab4f95aaa89a9ef885b5366d9b66b65f
Reilly Krause <reikwork@gmail.com>
2024-07-01 17:01:25 -0700

    Changes made in the NoteDialog to allow navigation from modal to full-screen editor; fixed issue with adding new note

--- src/lib/components/NoteDialog.svelte
+++ src/lib/components/NoteDialog.svelte
@@ -6,10 +6,11 @@
     import { t, selectedVerses, bodyFontSize, currentFont } from '$lib/data/stores';
     import { editNote, addNote } from '$lib/data/notes';
     import { goto } from '$app/navigation';
+    import { base } from '$app/paths';
+    import { stringify } from 'querystring';
 
     export let note = undefined;
     export let editing = false;
-    export let show;
 
     let id = 'note';
     let modal;
@@ -22,6 +23,7 @@
         if (note !== undefined) {
             text = note.text;
             title = 'Annotation_Note_Edit';
+            
         } else {
             editing = true;
             title = 'Annotation_Note_Add';
@@ -33,6 +35,28 @@
         text = '';
         editing = false;
         selectedVerses.reset();
+    }
+
+    async function createNote() {
+
+        console.log('Im here as well! selectedVerses: ', $selectedVerses[0].reference);
+
+        // const verses = $selectedVerses[0]; //TEST ------------------------------------------------------------------
+
+         const newNote = await addNote({
+                docSet: $selectedVerses[0].docSet,
+                collection: $selectedVerses[0].collection,
+                book: $selectedVerses[0].book,
+                chapter: $selectedVerses[0].chapter,
+                verse: $selectedVerses[0].verse,
+                text,
+                reference: $selectedVerses[0].reference
+            });
+        note = newNote;
+        console.log('Im here! note: ', note);
+        // console.log('Im here! selectedVerses: ', verses);
+        // console.log('Im here!', note);
+        goto(`${base}/notes/edit/${note.date}`);
     }
 
     async function modifyNote() {
@@ -53,40 +77,36 @@
             });
         }
     }
+    async function onEditNote(){
+        if (note !== undefined) {
+            console.log('Im here also! note: ', note);
+            goto(`${base}/notes/edit/${note.date}`);
+        } else {
+            createNote();
+        }
+    }
+
 </script>
 
 <Modal bind:this={modal} {id} on:close={reset} useLabel={false}>
     <svelte:fragment slot="content">
         <div id="container" class="flex flex-col justify-evenly">
-            <div class="w-full flex justify-between">
+            <div class="w-full flex justify-between items-center">
                 <div
-                    class="annotation-item-title w-full pb-3"
-                    style:font-weight={editing ? 'normal' : 'bold'}
+                    class="annotation-item-title w-full pb-3 font-bold"
                 >
                     {heading}
                 </div>
-                {#if !editing}
-                    <button
-                        on:click={() => {
-                            editing = true;
-                        }}
+                    <button class="dy-btn dy-btn-ghost dy-btn-circle"
+                        on:click={onEditNote}
                     >
                         <EditIcon />
                     </button>
-                {/if}
             </div>
-            <!-- TEST ----------------------------------------------------------------------- -->
-            {#if editing}
-                { goto(`/notes/edit/${note.date}`) }
-                
-            {/if}
+           
 
-            <!-- TEST ----------------------------------------------------------------------- -->
-
-            <!-- <div style:word-wrap="break-word">
-                {#if editing}
-                    <textarea bind:value={text} class="dy-textarea w-full" />
-                {:else if text !== undefined}
+            <div style:word-wrap="break-word" class="mt-2">
+                {#if text !== undefined}
                     {#each text.split(/\r?\n/) as line}
                         {#if line}
                             <p style:font-family={$currentFont} style:font-size="{$bodyFontSize}px">
@@ -98,14 +118,8 @@
                     {/each}
                 {/if}
             </div>
-            {#if editing}
-                <div class="w-full flex mt-4 justify-between">
-                    <button class="dy-btn dy-btn-sm dy-btn-ghost">{$t['Button_Cancel']}</button>
-                    <button on:click={modifyNote} class="dy-btn dy-btn-sm dy-btn-ghost"
-                        >{$t['Button_OK']}</button
-                    >
-                </div>
-            {/if} -->
         </div>
     </svelte:fragment>
 </Modal>
+
+ 
