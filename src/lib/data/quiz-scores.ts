868d54a2293755b8f92bc7a3fdb95f3b21b2f9d8
Aidan Scott <aidanpscott@gmail.com>
2024-07-03 10:51:20 -0400

    Create quiz-score.ts to handle quiz history functions

---
+++ src/lib/data/quiz-scores.ts
@@ -0,0 +1,68 @@
+import { openDB, type DBSchema } from 'idb';
+import config from '$lib/data/config';
+
+export interface HistoryItem {
+    date: number;
+    score: number;
+    collection: string;
+    book: string;
+    quiz: string;
+}
+
+interface History extends DBSchema {
+    history: {
+        key: number;
+        value: HistoryItem;
+        indexes: {
+            'collection, book, quiz': string;
+            date: number;
+        };
+    }
+}
+
+let historyDB = null;
+async function openHistory() {
+    if (!historyDB) {
+        historyDB = await openDB<History>('history', 1, {
+            upgrade(db) {
+                const historyStore = db.createObjectStore('history', {
+                    keyPath: 'date'
+                });
+                historyStore.createIndex('collection, book, quiz', [
+                    'collection',
+                    'book',
+                    'quiz'
+                ]);
+
+                historyStore.createIndex('date', 'date');
+            }
+        });
+    }
+    return historyDB;
+}
+
+export async function addHistory(item: {
+    collection: string;
+    book: string;
+    quiz: string;
+}) {
+    const history = await openHistory();
+    const date = new Date()[Symbol.toPrimitive]('number');
+    const bookIndex = config.bookCollections
+        .find((x) => x.id === item.collection)
+        .books.findIndex((x) => x.id === item.book)
+        .quizzes.findIndex((x) => x.id === item.quiz);
+    const nextItem = { ...item, date: date, bookIndex: bookIndex };
+    await history.add('history', nextItem);
+    //notifyUpdated();
+}
+
+export async function clearHistory() {
+    let history = await openHistory();
+    await history.clear('history');
+}
+
+export async function getHistory() {
+    const history = await openHistory();
+    return await history.getAllFromIndex('history', 'date');
+}
