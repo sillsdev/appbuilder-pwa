4daafe61e8017c3213512d79baed3c2c470a9b5b
Aidan Scott <aidanpscott@gmail.com>
2024-07-24 09:59:40 -0400

    BookSelector on Navbar displays quiz name for locked quizzes

--- src/routes/quiz/[collection]/[id]/+page.js
+++ src/routes/quiz/[collection]/[id]/+page.js
@@ -9,12 +9,11 @@ export async function load({ params, fetch }) {
 
     const book = config.bookCollections.find((x) => x.id === collection).books.find((x) => x.id === id);
 
-    //The problem with this is that it doesn't get data from the json file and creates error messages in the svelte page as a result
     if (book.quizFeatures['access-type'] === 'after') {
         const dependentQuizId = book.quizFeatures['access-after'];
         const accessGranted = await checkQuizAccess(dependentQuizId);
         if (!accessGranted) {
-            return { locked: true, dependentQuizId: dependentQuizId, quizId: id }; //write the condition such that there is no data. Make it more conditional: if the data doesn't exist, default to whatever
+            return { locked: true, dependentQuizId: dependentQuizId, quizId: id, quizName: book.name };
         }
     }
 
