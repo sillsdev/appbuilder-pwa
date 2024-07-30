50e31943f1640203b7c50e43f7ca63e4c0674bc5
Aidan Scott <aidanpscott@gmail.com>
2024-07-30 13:35:54 -0400

    Make some quiz svelte elements reactive so quizzes can switch from locked to unlocked properly

--- src/routes/quiz/[collection]/[id]/+page.svelte
+++ src/routes/quiz/[collection]/[id]/+page.svelte
@@ -17,16 +17,24 @@
     /** @type {import('./$types').PageData} */
     export let data;
 
-    let locked = data.locked;
-    let dependentQuizId = data.dependentQuizId;
-    let quiz = data.quiz || {};
+    $: ({ locked, dependentQuizId, quiz, quizId, quizName } = data);
+
+    $: book = config.bookCollections
+        .find((x) => x.id === $refs.collection)
+        .books.find((x) => x.id === quizId);
+
+    $: displayLabel = quizName || 'Quiz';
+
+    // let locked = data.locked;
+    // let dependentQuizId = data.dependentQuizId;
+    // let quiz = data.quiz || {};
     let textHighlightIndex = -1;
     console.log(data);
     console.log(data.quiz);
-    let book = config.bookCollections
-        .find((x) => x.id === $refs.collection)
-        .books.find((x) => x.id === quiz.id);
-    let displayLabel = book?.name || data?.quizName;
+    // let book = config.bookCollections
+    //     .find((x) => x.id === $refs.collection)
+    //     .books.find((x) => x.id === quiz.id);
+    // let displayLabel = book?.name || data?.quizName;
     let shuffledAnswers = [];
     let quizQuestions = [];
     let score = 0;
@@ -261,7 +269,7 @@
     }
 
     onMount(() => {
-        if (!locked) {
+        if (!locked && quiz) {
             if (book.quizFeatures['shuffle-questions']) {
                 shuffleQuestions();
             } else {
