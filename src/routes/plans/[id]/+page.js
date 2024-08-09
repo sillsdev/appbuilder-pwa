f5d9bab79668851261e4d7e292dea93be749784d
Mikayla <mikayla@thehubbards.org>
2024-08-09 14:19:54 -0400

    begin plans views: plan selection and plan detail
    done/started:
    - routing that takes you to the plan detail page
     when you click on a plan (only works with the
     plans under the choose plan tab - needs work)
     - initial set up of the plan detail page with
     two tabs: info and calendar
     - display data/ui for the info tab
     - display data/ui for the calendar tab
     - in calendar, clicking on a day will display
     the refs for that day
     note: the detail page was designed with the
     assumptiion that it's an unstarted plan
    
    to do/needs work:
    - databses and related issues
    - json is only showing the first word of a string
    - icons for the tabs in the detail page
    - checkbox image for references under the calendar
    - styling:
        - width isn't right
        - especially noticed on the calendar tab, the
        css styles are not being found/applied
        (I also added some css styles that were from
        the inspection of the android to test some
        things, and that should be taken out)
    - there are no examples with multiple refs in a
    day, so I'm not 100% sure that part of the code
    works as it was untestable

---
+++ src/routes/plans/[id]/+page.js
@@ -0,0 +1,40 @@
+
+import config from '../../../lib/data/config';
+import { base } from '$app/paths';
+
+export async function load({ params, fetch }) {
+    const allPlans = config.plans.plans;
+
+    const id = params.id;
+
+
+    const planConfig = allPlans.find((x) => x.id === id)
+
+    console.log('plan:', planConfig);
+
+
+    const src = `${base}/plans/${planConfig.jsonFilename}`;
+    let planData;
+
+    try {
+        const response = await fetch(src);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch plan JSON file');
+        }
+
+        planData = await response.json();
+    } catch (error) {
+        console.error('Error fetching plan JSON file:', error);
+    }
+
+
+    return { planConfig, planData };
+
+
+}
+
+
+
+
+
