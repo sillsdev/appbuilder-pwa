c83a791b3614ca619308d44999a7ead6ec655385
Chris Hubbard <chris@thehubbards.org>
2024-08-13 14:33:59 -0400

    Fix eslint changes

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        ignores: [
+            '.DS_Store',
+            'node_modules',
+            'build',
+            '.svelte-kit',
+            'package',
+            '.env',
+            '.env.*',
+            '!env.example',
+            'data',
+            'src/config.js',
+            'static',
+            'example_data',
+            'test_data',
+            'pnpm-lock.yaml',
+            'package-lock.json',
+            'yarn.lock'
+        ]
+    }
+];
