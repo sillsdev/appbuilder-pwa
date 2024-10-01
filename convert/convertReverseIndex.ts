42ceaa41eb6fa1730b5d4727790a77ec964bc8a4
Carson Kramer <carsondkramer@gmail.com>
2024-10-01 16:51:20 -0400

    Provide stub of convert in Reverse Index function

---
+++ convert/convertReverseIndex.ts
@@ -0,0 +1,19 @@
+import { TaskOutput, Task } from './Task';
+
+export function convertReverseIndex(dataDir: string, verbose: number) {
+    
+}
+export class ConvertReverseIndex extends Task {
+    public triggerFiles: string[] = [];
+
+    constructor(dataDir: string) {
+        super(dataDir);
+    }
+    public async run(verbose: number, outputs: Map<string, TaskOutput>): Promise<TaskOutput> {
+        convertReverseIndex(this.dataDir, verbose);
+        return {
+            taskName: this.constructor.name,
+            files: []
+        };
+    }
+}
