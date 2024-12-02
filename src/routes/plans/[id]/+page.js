f944077f0ca478a353c00ab17f94962aed95a926
davidmoore1 <david_moore1@sil.org>
2024-12-02 16:38:22 -0500

    fix syntax error

--- src/routes/plans/[id]/+page.js
+++ src/routes/plans/[id]/+page.js
@@ -1,6 +1,6 @@
 import config from '../../../lib/data/config';
 import { getPlanData } from '../../../lib/data/plansData';
-import { getAllProgressItemsForPlan } from '$lib/data/PlanProgressItems';
+import { getAllProgressItemsForPlan } from '$lib/data/planProgressItems';
 
 export async function load({ params, fetch }) {
     const allPlans = config.plans.plans;
