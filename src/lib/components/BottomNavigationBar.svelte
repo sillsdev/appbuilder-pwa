31cc2b7efa6fce40ce6149c6a85a2d3aaab7b418
davidmoore1 <david_moore1@sil.org>
2024-12-12 17:29:03 -0500

    again

--- src/lib/components/BottomNavigationBar.svelte
+++ src/lib/components/BottomNavigationBar.svelte
@@ -77,7 +77,10 @@ TODO:
                 goto(`${base}/settings`);
                 break;
             default:
-                console.log('BottomNavigation Bar: Unknown NavBar button type pressed: ', buttonType);
+                console.log(
+                    'BottomNavigation Bar: Unknown NavBar button type pressed: ',
+                    buttonType
+                );
                 break;
         }
     }
