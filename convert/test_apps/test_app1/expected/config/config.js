6766524ad914cfa316230543b860a0d970d40ee3
Garrett Jones <jones.garrett_m@outlook.com>
2025-01-02 12:50:47 -0600

    Add test coverage for additional book names

--- convert/test_apps/test_app1/expected/config/config.js
+++ convert/test_apps/test_app1/expected/config/config.js
@@ -1201,6 +1201,11 @@ export default {
           "fonts": [],
           "id": "MAT",
           "name": "Matthew",
+          "additionalNames": [
+            {
+              "name": "The First Gospel"
+            }
+          ],
           "section": "Gospel",
           "testament": "NT",
           "abbreviation": "Mat",
