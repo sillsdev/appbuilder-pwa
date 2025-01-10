e211047547fff75daa71b8db19617ee0b1067335
Chris Hubbard <chris@thehubbards.org>
2025-01-10 09:38:10 -0500

    Remove debug logging

--- src/lib/data/stores/scripture.js
+++ src/lib/data/stores/scripture.js
@@ -83,7 +83,7 @@ export async function getVerseText(item, item2 = undefined) {
                 ? `${item.chapter}:${item.verse}-${item2.verse}`
                 : `${item.chapter}:${item.verse}-${item2.chapter}:${item2.verse}`
             : `${item.chapter}:${item.verse}`;
-    console.log('getVerseText', scriptureCV);
+    //console.log('getVerseText', scriptureCV);
     const query = `{
       docSet (id: "${item.docSet}") {
           document(bookCode:"${item.book}") {
@@ -95,7 +95,7 @@ export async function getVerseText(item, item2 = undefined) {
           }
       }
   }`;
-    console.log(query);
+    //console.log(query);
 
     const { data } = await proskomma.gqlQuery(query);
     let text = [];
