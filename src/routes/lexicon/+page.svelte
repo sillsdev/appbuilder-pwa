a94997ee65d4fa41f774284d8914f9b59a618fb9
EthanFennell <edfennell@liberty.edu>
2025-02-15 16:49:04 -0500

    Changed use of "slot" to "render" for navbar

--- src/routes/lexicon/+page.svelte
+++ src/routes/lexicon/+page.svelte
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<script lang="ts">
-    import { page } from '$app/stores';
-    import Navbar from '$lib/components/Navbar.svelte';
-    import config from '$lib/data/config';
-
-    const { entries } = $page.data;
-</script>
-
-<div class="grid grid-rows-[auto,1fr]" style="height:100vh;height:100dvh;">
-    <div class="navbar">
-        <Navbar showBackButton={false}>
-            <!-- <div slot="left-buttons" /> -->
-            <label for="sidebar" slot="center">
-                <div class="btn btn-ghost normal-case text-xl">{config.name}</div>
-            </label>
-            <!-- <div slot="right-buttons" /> -->
-        </Navbar>
-    </div>
-    <div class="overflow-y-auto max-w-screen-md">
-        <div id="container" class="lexicon">
-            <ul>
-                {#if entries.length > 0}
-                    {#each entries as entry}
-                        <li>{entry.name}</li>
-                    {/each}
-                {:else}
-                    <li>No entries available or table 'entries' not found.</li>
-                {/if}
-            </ul>
-        </div>
-    </div>
-</div>
-=======
 <script>
     import { goto } from '$app/navigation';
     import { base } from '$app/paths';
@@ -225,11 +191,13 @@
 </script>
 
 <Navbar>
-    <label for="sidebar" slot="center" class="navbar">
+    {#snippet center()}
+    <label for="sidebar" class="navbar">
         <div class="btn btn-ghost normal-case text-xl text-white font-bold">
             {dictionaryName}
         </div>
     </label>
+    {/snippet}
 </Navbar>
 
 <div class="flex flex-col">
@@ -271,4 +239,3 @@
         </ul>
     {/if}
 </div>
->>>>>>> 8041142 (Create route for lexicon page)
