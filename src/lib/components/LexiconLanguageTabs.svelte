25d84845b3269ef8f67734365906077ed83420e0
EthanFennell <edfennell@liberty.edu>
2025-04-11 16:41:07 -0400

    Fix spacing

--- src/lib/components/LexiconLanguageTabs.svelte
+++ src/lib/components/LexiconLanguageTabs.svelte
@@ -1,6 +1,6 @@
 <script>
     import { expoInOut } from 'svelte/easing';
-    import { fly, slide } from 'svelte/transition';
+    import { fly } from 'svelte/transition';
 
     export let reversalLanguage;
     export let selectedLanguage;
@@ -15,13 +15,12 @@
         aria-pressed={selectedLanguage === vernacularLanguage}
         on:click={() => onSwitchLanguage(vernacularLanguage)}
         on:keydown={(e) => e.key === 'Enter' && onSwitchLanguage(vernacularLanguage)}
-        class="py-3.5 px-3.5 text-sm uppercase text-center relative dy-tabs dy-tabs-bordered mb-1 cursor-pointer"
-    >
+        class="py-2.5 px-3.5 text-sm uppercase text-center relative dy-tabs dy-tabs-bordered mb-1">
         {vernacularLanguage}
         {#if selectedLanguage === vernacularLanguage}
             <div
-                transition:fly={{ axis: 'x', easing: expoInOut, x: 70 }}
-                class="absolute bottom-0 left-0 w-full h-1 bg-black"
+                transition:fly="{{ axis: 'x', easing: expoInOut, x: 70 }}"
+                class="absolute -bottom-1 left-0 w-full h-1 bg-black"
             ></div>
         {/if}
     </div>
@@ -31,13 +30,13 @@
         aria-pressed={selectedLanguage === reversalLanguage}
         on:click={() => onSwitchLanguage(reversalLanguage)}
         on:keydown={(e) => e.key === 'Enter' && onSwitchLanguage(reversalLanguage)}
-        class="py-3.5 px-3.5 text-sm uppercase text-center relative dy-tabs dy-tabs-bordered mb-1 cursor-pointer"
+        class="py-2.5 px-3.5 text-sm uppercase text-center relative dy-tabs dy-tabs-bordered mb-1"
     >
         {reversalLanguage}
         {#if selectedLanguage === reversalLanguage}
             <div
-                transition:fly={{ axis: 'x', easing: expoInOut, x: -70 }}
-                class="absolute bottom-0 left-0 w-full h-1 bg-black"
+                transition:fly="{{ axis: 'x', easing: expoInOut, x: -70 }}"
+                class="absolute -bottom-1 left-0 w-full h-1 bg-black"
             ></div>
         {/if}
     </div>
