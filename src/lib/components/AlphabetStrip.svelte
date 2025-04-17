7518a06dfa250c695ca5374653dc05a7be7de315
Carson Kramer <carsondkramer@gmail.com>
2025-04-17 11:38:51 -0400

    Fix wide screen gap

--- src/lib/components/AlphabetStrip.svelte
+++ src/lib/components/AlphabetStrip.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-    class="flex m-2 gap-1 md:gap-2 mb-4 justify-start overflow-x-auto whitespace-nowrap pb-2 snap-x"
+    class="flex m-2 gap-1 md:gap-2 mb-4 justify-start overflow-x-auto whitespace-nowrap pb-2 snap-x min-w-[100vw]"
 >
     {#each alphabet as letter}
         <button
