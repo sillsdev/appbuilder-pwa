93d09083cd0ade30ba535b09c551af0d23f49583
Carson Kramer <carsondkramer@gmail.com>
2025-05-12 11:55:18 -0400

    Fix formatting

--- src/routes/lexicon/search/+page.svelte
+++ src/routes/lexicon/search/+page.svelte
@@ -129,7 +129,7 @@
             <div class="flex justify-center">
                 <div class="flex-1 overflow-auto justify-center px-4 w-full max-w-screen-md p-4">
                     {#if wordIds && wordIds.length > 0}
-                        <LexiconEntryView {wordIds} onSelectWord={selectWord} removeNewLines/>
+                        <LexiconEntryView {wordIds} onSelectWord={selectWord} removeNewLines />
                     {:else if wordIds && wordIds.length == 0}
                         <div class="text-center" style="color: var(--SettingsSummaryColor);">
                             No results found.
