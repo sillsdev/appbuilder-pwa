973dd3ad93770a1431fa10b3d538e0a72ef2d2a4
Joshua Kroeker <62664962+TheNonPirate@users.noreply.github.com>
2025-06-10 08:56:00 -0600

    Make a preventDefault wrapper function (#825)
    * Made a preventDefault wrapper function
    
    * Slightly modified event-wrappers.ts to conform to Prettier

---
+++ src/lib/scripts/event-wrappers.ts
@@ -0,0 +1,6 @@
+export function preventDefault(fn: (event: Event) => void): (event: Event) => void {
+    return function (event: Event) {
+        event.preventDefault();
+        fn.call(this, event);
+    };
+}
