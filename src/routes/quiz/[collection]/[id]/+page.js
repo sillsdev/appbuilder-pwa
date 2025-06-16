88a0fe262c6277d50d1919492be122c1bcf33b33
Chris Hubbard <chris@thehubbards.org>
2025-06-16 16:17:51 -0400

    Restore the quiz page.js (overwritten by Dictionary PWA) (#841)

--- src/routes/quiz/[collection]/[id]/+page.js
+++ src/routes/quiz/[collection]/[id]/+page.js
@@ -1,33 +1,53 @@
 import { base } from '$app/paths';
 import config from '$lib/data/config';
+import { checkQuizAccess } from '$lib/data/quiz';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-    if (!config.writingSystems) {
-        throw new Error('Writing systems configuration not found');
+export async function load({ params, fetch }) {
+    const id = params.id;
+    const collection = params.collection;
+
+    const bookCollection = config.bookCollections.find((x) => x.id === collection);
+    const book = bookCollection.books.find((x) => x.id === id);
+
+    let locked = false;
+    let dependentQuizId = null;
+    let dependentQuizName = null;
+
+    if (book.quizFeatures['access-type'] === 'after') {
+        dependentQuizId = book.quizFeatures['access-after'];
+        const accessGranted = await checkQuizAccess(dependentQuizId);
+        locked = !accessGranted;
     }
 
-    const [defaultKey, writingSystem] = Object.entries(config.writingSystems)[0];
-
-    if (!writingSystem?.alphabet) {
-        throw new Error('Writing system alphabet not found');
+    if (locked && dependentQuizId) {
+        const dependentBook = bookCollection.books.find((x) => x.id === dependentQuizId);
+        dependentQuizName = dependentBook ? dependentBook.name : dependentQuizId;
     }
 
-    const alphabet = writingSystem.alphabet;
+    let quizData;
+    let passScore = 0;
+    if (!locked) {
+        try {
+            const response = await fetch(`${base}/collections/${collection}/quizzes/${id}.json`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch quiz JSON file');
+            }
 
-    let initialReversalData = {};
-    try {
-        const response = await fetch(`${base}/reversal/language/${defaultKey}/${alphabet[0]}.json`);
-        if (response.ok) {
-            initialReversalData = await response.json();
+            quizData = await response.json();
+            passScore = quizData.passScore;
+        } catch (error) {
+            console.error('Error fetching quiz JSON file:', error);
         }
-    } catch (error) {
-        console.error('Error loading initial reversal data:', error);
     }
 
     return {
-        alphabet,
-        initialReversalData,
-        defaultKey
+        quiz: quizData,
+        locked,
+        quizId: id,
+        quizName: book.name,
+        dependentQuizId,
+        dependentQuizName,
+        passScore
     };
 }
