96885d35c3b8e5ae301c0802cd80ec935a2ca961
Judah Sotomayor <judah.sotomayor@pm.me>
2025-06-23 09:31:07 -0400

    Add a delete button to the select note button.
    Previously users had to click through to the editing dialog to delete a
    note.
    This change introduces a trash button next to the edit button to allow
    deleting a note with just two clicks.

--- src/lib/components/NoteDialog.svelte
+++ src/lib/components/NoteDialog.svelte
@@ -1,17 +1,16 @@
 <svelte:options accessors={true} />
 
 <script lang="ts">
-    import { addNote, editNote } from '$lib/data/notes';
-    import { EditIcon } from '$lib/icons';
-    import Modal from './Modal.svelte';
-    import { t, selectedVerses, bodyFontSize, currentFont } from '$lib/data/stores';
     import { goto } from '$app/navigation';
-    import { getRoute } from '$lib/navigate';
     import { base } from '$app/paths';
+    import { addNote, editNote, removeNote } from '$lib/data/notes';
+    import { bodyFontSize, currentFont, selectedVerses, t } from '$lib/data/stores';
+    import { DeleteIcon, EditIcon } from '$lib/icons';
+    import { getRoute } from '$lib/navigate';
+    import Modal from './Modal.svelte';
 
     export let note = undefined;
     export let editing = false;
-
 
     let id = 'note';
     let modal;
@@ -36,41 +35,39 @@
     function onEditNote() {
         if (note !== undefined) goto(getRoute(`/notes/edit/${note.date}`));
     }
+
+    function onDeleteNote() {
+        removeNote(note.date);
+    }
 </script>
 
 <Modal bind:this={modal} {id} onclose={reset}>
-        <div class="flex flex-col justify-evenly">
-            <div class="w-full flex justify-between items-center">
-                <div class="w-full pb-3" style:font-weight={editing ? 'normal' : 'bold'}>
-                    {heading}
-                </div>
-                    <button class="dy-btn dy-btn-ghost dy-btn-circle" onclick={onEditNote}>
-                        <EditIcon />
-                    </button>
+    <div class="flex flex-col justify-evenly">
+        <div class="w-full flex justify-between items-center">
+            <div class="w-full pb-3" style:font-weight="bold">
+                {heading}
             </div>
-
-            <div style:word-wrap="break-word" class="mt-2">
-            <!-- TODO Check if this first if-statement is still necessary -->
-                {#if text !== undefined}
-                    {#each text.split(/\r?\n/) as line}
-                        {#if line}
-                            <p style:font-family={$currentFont} style:font-size="{$bodyFontSize}px">
-                                {line}
-                            </p>
-                        {:else}
-                            <br />
-                        {/if}
-                    {/each}
-                {/if}
-
-            <!--  </div> -->
-<!--            {#if editing}
-                <div class="w-full flex mt-4 justify-between">
-                    <button class="dy-btn dy-btn-sm dy-btn-ghost">{$t['Button_Cancel']}</button>
-                    <button onclick={modifyNote} class="dy-btn dy-btn-sm dy-btn-ghost"
-                        >{$t['Button_OK']}</button
-                    >
-                </div>
-            {/if}-->
+            <button class="dy-btn dy-btn-ghost dy-btn-circle" onclick={onDeleteNote}>
+                <DeleteIcon />
+            </button>
+            <button class="dy-btn dy-btn-ghost dy-btn-circle" onclick={onEditNote}>
+                <EditIcon />
+            </button>
         </div>
-</Modal>
+
+        <div style:word-wrap="break-word" class="mt-2">
+            <!-- TODO Check if this first if-statement is still necessary -->
+            {#if text !== undefined}
+                {#each text.split(/\r?\n/) as line}
+                    {#if line}
+                        <p style:font-family={$currentFont} style:font-size="{$bodyFontSize}px">
+                            {line}
+                        </p>
+                    {:else}
+                        <br />
+                    {/if}
+                {/each}
+            {/if}
+        </div>
+    </div></Modal
+>
