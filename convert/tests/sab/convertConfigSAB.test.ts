a237e2bc3ecb9734d23873854ef9869a1e580de7
Joshua Kroeker <62664962+TheNonPirate@users.noreply.github.com>
2025-07-02 07:35:45 -0600

    Implementing BookTabs (#855)
    * Caused convertConfig to convert book tab data from data/appdef.xml to src/lib/data/config.js. Also created the relevant types in index.d.ts.
    
    * Made code Prettier-compliant
    
    * Added tests for bookTab parsing
    
    * Fixed issues with Prettier syntax compliance in convertConfigSAB.test.ts
    
    * Fixed a typo in convertConfig.ts that resulted in a value in bookTabs coming from the book rather than the book tab.
    
    * Remove dev comments
    
    ---------
    
    Co-authored-by: Chris Hubbard <chris@thehubbards.org>

--- convert/tests/sab/convertConfigSAB.test.ts
+++ convert/tests/sab/convertConfigSAB.test.ts
@@ -126,6 +126,35 @@ if (programType === 'DAB') {
                         (r) => r === '' || r === undefined
                     );
                 }
+                if (bkk.bookTabs) {
+                    expect(bkk.bookTabs.mainType).not.toSatisfy((r) => r === '' || r === undefined);
+                    expect(bkk.bookTabs.tabs).not.toHaveLength(0);
+                    for (const bookTab in bkk.bookTabs.tabs) {
+                        const bt = bkk.bookTabs.tabs[bookTab];
+                        expect(bt.type).not.toSatisfy((r) => r === '' || r === undefined);
+                        expect(bt.file).not.toSatisfy((r) => r === '' || r === undefined);
+                        for (const audio in bt.audio) {
+                            expect(bt.audio[audio].num).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                            expect(bt.audio[audio].filename).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                            expect(bt.audio[audio].len).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                            expect(bt.audio[audio].size).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                            expect(bt.audio[audio].src).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                            expect(bt.audio[audio].timingFile).not.toSatisfy(
+                                (r) => r === '' || r === undefined
+                            );
+                        }
+                    }
+                }
             }
 
             expect(Object.keys(bc.style)).toHaveLength(6);
