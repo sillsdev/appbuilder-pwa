59c873cbe3a9399acb13fa426c840c773acc07f0
Chris Hubbard <chris@thehubbards.org>
2025-07-29 10:10:50 -0400

    Fix BookTab theming

--- src/lib/components/BookTabs.svelte
+++ src/lib/components/BookTabs.svelte
@@ -7,7 +7,7 @@ TODO:
 -->
 <script>
     import config from '$lib/data/config';
-    import { refs, themeColors } from '$lib/data/stores';
+    import { convertStyle, monoIconColor, refs, s } from '$lib/data/stores';
     import MusicIcon from '$lib/icons/MusicIcon.svelte';
     import NotesIcon from '$lib/icons/NotesIcon.svelte';
     import QuestionsIcon from '$lib/icons/QuestionsIcon.svelte';
@@ -17,9 +17,8 @@ TODO:
     const bookTabs = $derived(
         config?.bookCollections
             .find((x) => x.id === $refs.collection)
-            .books.find((x) => x.id === $refs.book)?.bookTabs
+            ?.books.find((x) => x.id === $refs.book)?.bookTabs
     );
-    const primaryColor = $state($themeColors.PrimaryColor);
     const icons = {
         T: TextIcon,
         Q: QuestionsIcon,
@@ -34,34 +33,22 @@ TODO:
     }
 </script>
 
-<div class="dy-tabs dy-tabs-border w-full">
-    {#if $refs.bookTab === 0 || $refs.bookTab === undefined}
-        <div
-            class="dy-tab text-center dy-btn rounded-none border-primary border-b-5 border-x-0 border-t-0"
-        >
-            <MainIcon color={primaryColor}></MainIcon>
-        </div>
-    {:else}
-        <button class="dy-tab text-center dy-btn rounded-none" onclick={() => changeTab(0)}>
-            <MainIcon color={primaryColor}></MainIcon>
-        </button>
-    {/if}
+<div class="dy-tabs dy-tabs-bordered mx-auto max-w-screen-md w-full">
+    <button
+        class="dy-tab {$refs.bookTab === 0 || $refs.bookTab === undefined ? 'dy-tab-active' : ''}"
+        onclick={() => changeTab(0)}
+        style={convertStyle($s['ui.book.tabs'])}
+    >
+        <MainIcon color={$monoIconColor}></MainIcon>
+    </button>
     {#each bookTabs.tabs as bookTab, i}
         {@const Icon = icons[bookTab.type]}
-        {#if $refs.bookTab === i + 1}
-            <div
-                class="dy-tab text-center dy-btn rounded-none border-primary border-b-5 border-x-0 border-t-0"
-            >
-                <Icon color={primaryColor}></Icon>
-            </div>
-        {:else}
-            <button class="dy-tab text-center dy-btn rounded-none" onclick={() => changeTab(i + 1)}>
-                <Icon color={primaryColor}></Icon>
-            </button>
-        {/if}
+        <button
+            class="dy-tab {$refs.bookTab === i + 1 ? 'dy-tab-active' : ''} "
+            onclick={() => changeTab(i + 1)}
+            style={convertStyle($s['ui.book.tabs'])}
+        >
+            <Icon color={$monoIconColor}></Icon>
+        </button>
     {/each}
-    <!--
-    <div class="dy-tab text-center dy-btn rounded-none">Tab 2</div>
-    <div class="dy-tab text-center dy-btn rounded-none">Tab 3</div>
--->
 </div>
