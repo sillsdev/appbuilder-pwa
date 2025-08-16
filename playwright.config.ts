994b54b16e2e9df0276820e8e0aa57574c2976d0
AslanRules <39661498+AslanRules@users.noreply.github.com>
2025-08-16 11:14:16 -0400

    rename files fixing vitest/playwright conflict

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
     testDir: './src/lib/scripts/playwright',
+    /* Changing file matching pattern to not conflict with Vitest */
+    testMatch: '*.e2e.ts',
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
