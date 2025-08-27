e42e38c455c5001d899209822ebea1a6dbb68c07
Chris Hubbard <chris@thehubbards.org>
2025-08-27 10:56:46 -0400

    Migrate Plans to Svelte 5 (#893)
    * Update Plans to Svelte 5
    
    * Fix effect; Use asset

--- src/routes/plans/+page.svelte
+++ src/routes/plans/+page.svelte
@@ -1,62 +1,49 @@
 <script>
-    import { base } from '$app/paths';
-    import { page } from '$app/stores';
+    import { asset } from '$app/paths';
     import BottomNavigationBar from '$lib/components/BottomNavigationBar.svelte';
     import Navbar from '$lib/components/Navbar.svelte';
     import config from '$lib/data/config';
     import { getLastPlanState } from '$lib/data/planStates';
-    import {
-        convertStyle,
-        language,
-        modal,
-        MODAL_COLLECTION,
-        MODAL_TEXT_APPEARANCE,
-        s,
-        t,
-        themeColors
-    } from '$lib/data/stores';
+    import { convertStyle, language, s, t } from '$lib/data/stores';
     import { gotoRoute } from '$lib/navigate';
     import { compareVersions } from '$lib/scripts/stringUtils';
 
     const imageFolder =
         compareVersions(config.programVersion, '12.0') < 0 ? 'illustrations' : 'plans';
 
-    //filter out the ones that are in $data.plans, looking at id
-    // $: availablePlans =
-    // //what plans are viewed based on what tab is selected
-    // $: viewedPlans =
-
-    let selectedTab = 'available';
-
-    let availablePlans = [];
-    let completedPlans = [];
-    let usedPlans = [];
+    let selectedTab = $state('available');
+    let availablePlans = $state([]);
+    let completedPlans = $state([]);
+    let usedPlans = $state([]);
     let allPlans = config.plans.plans || [];
-    let plansInUse = [];
-    const promises = allPlans.map((plan) =>
-        getLastPlanState(plan.id)
-            .then((planState) => {
-                if (planState && planState === 'started') {
-                    plansInUse = [...plansInUse, plan];
-                }
-                if (planState && planState === 'completed') {
-                    completedPlans = [...completedPlans, plan];
-                }
-            })
-            .catch(console.error)
-    );
-    Promise.all(promises).then(() => {
-        if (plansInUse.length > 0) {
-            selectedTab = 'in-use';
-        }
+    let plansInUse = $state([]);
+
+    $effect(() => {
+        const promises = allPlans.map((plan) =>
+            getLastPlanState(plan.id)
+                .then((planState) => {
+                    if (planState && planState === 'started') {
+                        plansInUse = [...plansInUse, plan];
+                    }
+                    if (planState && planState === 'completed') {
+                        completedPlans = [...completedPlans, plan];
+                    }
+                })
+                .catch(console.error)
+        );
+        Promise.all(promises).then(() => {
+            if (plansInUse.length > 0) {
+                selectedTab = 'in-use';
+            }
+        });
     });
 
-    $: {
+    $effect(() => {
         availablePlans = allPlans.filter(
             (plan) => !plansInUse.some((usedPlan) => usedPlan.id === plan.id)
         );
         usedPlans = plansInUse;
-    }
+    });
     const bottomNavBarEnabled = config?.bottomNavBarItems && config?.bottomNavBarItems.length > 0;
     const barType = 'plans';
 </script>
@@ -84,7 +71,7 @@
                     name="my_tabs_1"
                     role="tab"
                     class="dy-tab dy-tab-bordered {selectedTab === 'in-use' ? 'dy-tab-active' : ''}"
-                    on:click={() => (selectedTab = 'in-use')}
+                    onclick={() => (selectedTab = 'in-use')}
                     aria-label={$t['Plans_Tab_My_Plans']}
                     style={convertStyle($s['ui.plans.tabs.text'])}
                 />
@@ -94,7 +81,7 @@
                 name="my_tabs_1"
                 role="tab"
                 class="dy-tab {selectedTab === 'available' ? 'dy-tab-active' : ''}"
-                on:click={() => (selectedTab = 'available')}
+                onclick={() => (selectedTab = 'available')}
                 aria-label={$t['Plans_Tab_Choose_Plan']}
                 style={convertStyle($s['ui.plans.tabs.text'])}
             />
@@ -106,7 +93,7 @@
                     class="dy-tab dy-tab-bordered {selectedTab === 'completed'
                         ? 'dy-tab-active'
                         : ''}"
-                    on:click={() => (selectedTab = 'completed')}
+                    onclick={() => (selectedTab = 'completed')}
                     aria-label={$t['Plans_Tab_Completed_Plans']}
                     style={convertStyle($s['ui.plans.tabs.text'])}
                 />
@@ -118,18 +105,18 @@
                 <ul>
                     {#each availablePlans as plan}
                         <!-- add on click -->
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <!-- svelte-ignore a11y-no-static-element-interactions -->
+                        <!-- svelte-ignore a11y_click_events_have_key_events -->
+                        <!-- svelte-ignore a11y_no_static_element_interactions -->
                         <div
                             class="plan-chooser-plan plan-chooser-link"
                             id={plan.id}
-                            on:click={() => gotoRoute(`/plans/${plan.id}`)}
+                            onclick={() => gotoRoute(`/plans/${plan.id}`)}
                         >
                             {#if plan.image}
                                 <div class="plan-image-block">
                                     <img
                                         class="plan-image"
-                                        src="{base}/{imageFolder}/{plan.image.file}"
+                                        src={asset(`/${imageFolder}/${plan.image.file}`)}
                                         alt={plan.image.file}
                                         width={plan.image.width}
                                         height={plan.image.height}
@@ -150,18 +137,18 @@
             {:else if selectedTab === 'in-use'}
                 <ul>
                     {#each usedPlans as plan}
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <!-- svelte-ignore a11y-no-static-element-interactions -->
+                        <!-- svelte-ignore a11y_click_events_have_key_events -->
+                        <!-- svelte-ignore a11y_no_static_element_interactions -->
                         <div
                             class="plan-chooser-plan plan-chooser-link"
                             id={plan.id}
-                            on:click={() => gotoRoute(`/plans/${plan.id}`)}
+                            onclick={() => gotoRoute(`/plans/${plan.id}`)}
                         >
                             {#if plan.image}
                                 <div class="plan-image-block">
                                     <img
                                         class="plan-image"
-                                        src="{base}/{imageFolder}/{plan.image.file}"
+                                        src={asset(`/${imageFolder}/${plan.image.file}`)}
                                         alt={plan.image.file}
                                         width={plan.image.width}
                                         height={plan.image.height}
@@ -182,18 +169,18 @@
             {:else if selectedTab === 'completed'}
                 <ul>
                     {#each completedPlans as plan}
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <!-- svelte-ignore a11y-no-static-element-interactions -->
+                        <!-- svelte-ignore a11y_click_events_have_key_events -->
+                        <!-- svelte-ignore a11y_no_static_element_interactions -->
                         <div
                             class="plan-chooser-plan plan-chooser-link"
                             id={plan.id}
-                            on:click={() => gotoRoute(`/plans/${plan.id}`)}
+                            onclick={() => gotoRoute(`/plans/${plan.id}`)}
                         >
                             {#if plan.image}
                                 <div class="plan-image-block">
                                     <img
                                         class="plan-image"
-                                        src="{base}/{imageFolder}/{plan.image.file}"
+                                        src={asset(`/${imageFolder}/${plan.image.file}`)}
                                         alt={plan.image.file}
                                         width={plan.image.width}
                                         height={plan.image.height}
