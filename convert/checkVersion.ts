d98bb6c5a00d19eb2bf6fa4c4a1cca029d7fe492
Aidan Jones <aejones4gm@gmail.com>
2025-11-21 16:57:09 -0600

    Show node version on version check failure (#910)
    * Log node version
    
    * Format messages
    
    * Remove unneeded brackets from regex

--- convert/checkVersion.ts
+++ convert/checkVersion.ts
@@ -3,17 +3,18 @@ import { readFileSync } from 'node:fs';
 import { exit } from 'node:process';
 import { compareVersions } from './stringUtils';
 
-const fromCLI =
-    execSync('node --version')
-        .toString()
-        .match(/[\d]+(\.[\d]+){0,2}/)
-        ?.at(0) ?? '';
-const fromPackage = JSON.parse(readFileSync('package.json').toString()).volta.node;
+const verNumRegExp = /\d+(\.\d+){0,2}/;
+
+const fromCLI = execSync('node --version').toString().match(verNumRegExp)?.at(0) ?? '';
+const fromPackage =
+    JSON.parse(readFileSync('package.json').toString()).volta.node.match(verNumRegExp)?.at(0) ?? '';
 
 if (compareVersions(fromCLI, fromPackage) < 0) {
     console.error(
         `❌ Error: The PWA requires a newer version of NodeJS. Please use AppBuilders 13.3 or newer.`
     );
+    console.error(`| Environment: Node v${fromCLI}`);
+    console.error(`> Required: Node >= v${fromPackage}`);
     exit(1);
 } else {
     console.log(`✅ Using Node v${fromCLI}`);
