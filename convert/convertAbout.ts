142d3ed47b560586beeb7e6174c7bde51f572448
Aidan Jones <aidan_jones@sil.org>
2025-12-15 09:40:08 -0600

    Revert "Add caching support for illustrations in the about page (#919)"
    This reverts commit efe42b7e4d28aacf9fc4304b376ec7ed7419fce0.

--- convert/convertAbout.ts
+++ convert/convertAbout.ts
@@ -1,53 +1,20 @@
-import * as fs from 'fs';
+import { copyFile } from 'fs';
 import path from 'path';
-import { createHashedFile } from './fileUtils';
 import { Task, TaskOutput } from './Task';
-
-const base = process.env.BUILD_BASE_PATH || '';
 
 export interface AboutTaskOutput extends TaskOutput {
     taskName: 'ConvertAbout';
 }
-
-// Function to check if an image is missing
-function isImageMissing(imageSource: string, dataDir: string): boolean {
-    return !fs.existsSync(path.join(dataDir, 'illustrations', imageSource));
-}
-
-function updateImgTags(text: string, dataDir: string, verbose: number): string {
-    return text.replace(
-        /<img\b[^>]*\bsrc=["']([^"']*\/)?([^"']*)["'][^>]*>/gi,
-        (_match, _path, fileName) => {
-            // If the image is missing in the "illustrations" folder, filter out the entire tag
-            if (isImageMissing(fileName, dataDir)) {
-                return '';
-            } else {
-                fs.mkdirSync(path.join('static', 'illustrations'), { recursive: true });
-                const imagePath = createHashedFile(dataDir, `illustrations/${fileName}`, verbose);
-
-                return `<img src="${base}/${imagePath}">`;
-            }
-        }
-    );
-}
-
 /**
- * Copies about.partial.html to static folder with image tag processing
+ * Copies about.partial.html to static folder
  */
 export function convertAbout(dataDir: string, verbose: number) {
     const srcFile = path.join(dataDir, 'about.partial.html');
     const dstFile = path.join('src/gen-assets', 'about.partial.html');
-
-    // Read the file content
-    const content = fs.readFileSync(srcFile, 'utf8');
-
-    // Apply the updateImgTags filter
-    const processedContent = updateImgTags(content, dataDir, verbose);
-
-    // Write the processed content to the destination
-    fs.writeFileSync(dstFile, processedContent, 'utf8');
-
-    if (verbose) console.log(`copied and processed ${srcFile} to ${dstFile}`);
+    copyFile(srcFile, dstFile, function (err: any) {
+        if (err) throw err;
+        if (verbose) console.log(`copied ${srcFile} to ${dstFile}`);
+    });
 }
 export class ConvertAbout extends Task {
     public triggerFiles: string[] = ['about.partial.html'];
