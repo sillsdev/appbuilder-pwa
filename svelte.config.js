b24a38d60d453726231a6608ea750721c58a21d7
Chris Hubbard <chris@thehubbards.org>
2026-01-16 13:51:56 -0500

    Fix scope path to end with slash (#927)
    * Fix scope path to end with slash
    
    * Make scope and start_url consistent
    
    Based on ChatGPT feedback:
    https://chatgpt.com/share/696a8487-ece8-8006-a032-9a4d0230719e

--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,14 @@ const packageJson = JSON.parse(readFileSync('package.json', 'utf-8'));
 const templateVersion = packageJson.version;
 const buildTimestamp = Date.now();
 
+// Scope should end with a slash if defined
+// BUILD_BASE_PATH should not have a trailing slash, but just make sure
+const serviceWorkerScope = process.env.BUILD_BASE_PATH
+    ? process.env.BUILD_BASE_PATH.endsWith('/')
+        ? process.env.BUILD_BASE_PATH
+        : process.env.BUILD_BASE_PATH + '/'
+    : '/';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
@@ -34,7 +42,7 @@ const config = {
         },
         serviceWorker: {
             options: {
-                scope: process.env.BUILD_BASE_PATH
+                scope: serviceWorkerScope
             }
         },
         version: {
