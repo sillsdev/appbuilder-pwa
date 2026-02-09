403e09af2941d7aefa92ffb4d1b7de66bf60807b
Dave Smith <davidw_smith@sil.org>
2026-02-09 08:17:32 -0500

    Round 2 of Code Rabbit comments

--- src/lib/components/ContentHeading.svelte
+++ src/lib/components/ContentHeading.svelte
@@ -30,7 +30,7 @@
     function renderLastTextBox(layout: string | undefined): boolean {
         if (layout === undefined) return false;
         const exclude: Array<string> = ['image-left-text-right', 'image-right-text-left'];
-        console.log(`renderLastTextBox: ${exclude.find((x) => x === layout).length === 0}`);
+        console.log(`renderLastTextBox: ${exclude.find((x) => x === layout)?.length === 0}`);
         return !exclude.includes(layout);
     }
 </script>
