9d8cf843cc58ccaf506b5b75e60536d42fd293a8
Aidan Jones <aidan_jones@sil.org>
2026-02-23 13:52:53 -0600

    Address coderabbit feedback

--- convert/fetchTags.ts
+++ convert/fetchTags.ts
@@ -1,3 +1,4 @@
+import { exit } from 'node:process';
 import { compareVersions } from './stringUtils';
 
 const verNumRegExp = /^\d+(\.\d+){0,2}$/;
@@ -11,28 +12,36 @@ async function getDigest(token: string, ref: string) {
 }
 
 (async () => {
-    const token = (
-        await fetch('https://ghcr.io/token?scope=repository:sillsdev/app-builders:pull').then((r) =>
-            r.json()
-        )
-    ).token as string;
+    try {
+        const token = (
+            await fetch('https://ghcr.io/token?scope=repository:sillsdev/app-builders:pull').then(
+                (r) => r.json()
+            )
+        ).token as string;
 
-    const tags = (
-        await fetch('https://ghcr.io/v2/sillsdev/app-builders/tags/list', {
-            headers: { Authorization: `Bearer ${token}` }
-        }).then((r) => r.json())
-    ).tags as string[];
+        const tags = (
+            await fetch('https://ghcr.io/v2/sillsdev/app-builders/tags/list', {
+                headers: { Authorization: `Bearer ${token}` }
+            }).then((r) => r.json())
+        ).tags as string[];
 
-    const sortedTags = tags.filter((t) => t.match(verNumRegExp)).sort(compareVersions);
+        const sortedTags = tags.filter((t) => t.match(verNumRegExp)).sort(compareVersions);
 
-    const latestDigest = await getDigest(token, 'latest');
+        const latestDigest = await getDigest(token, 'latest');
 
-    let i = sortedTags.length;
-    let digest = '';
-    do {
-        i--;
-        digest = await getDigest(token, sortedTags[i]);
-    } while (i > 0 && latestDigest !== digest);
+        let i = sortedTags.length;
+        let digest = '';
+        do {
+            i--;
+            digest = await getDigest(token, sortedTags[i]);
+        } while (i > 0 && latestDigest !== digest);
 
-    console.log(`appbuilder_version=${sortedTags[i]}`);
-})();
+        console.log(`appbuilder_version=${sortedTags[i]}`);
+    } catch (e) {
+        console.error(e);
+        exit(1);
+    }
+})().catch((r) => {
+    console.error(r);
+    exit(1);
+});
