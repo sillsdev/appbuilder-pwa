d1e702258cc22396a66b9023162dbf30b94a220e
Aidan Jones <aidan_jones@sil.org>
2026-02-23 13:52:00 -0600

    Compare manifests to get the right tag

--- convert/fetchTags.ts
+++ convert/fetchTags.ts
@@ -1,22 +1,38 @@
 import { compareVersions } from './stringUtils';
 
-const verNumRegExp = /\d+(\.\d+){0,2}/;
+const verNumRegExp = /^\d+(\.\d+){0,2}$/;
+
+async function getDigest(token: string, ref: string) {
+    return (
+        await fetch(`https://ghcr.io/v2/sillsdev/app-builders/manifests/${ref}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((r) => r.json())
+    ).config.digest as string;
+}
 
 (async () => {
-    const tokenJSON = await fetch(
-        'https://ghcr.io/token?scope=repository:sillsdev/app-builders:pull'
-    ).then((r) => r.json());
-    const token = tokenJSON.token;
-    const tagJSON = await fetch('https://ghcr.io/v2/sillsdev/app-builders/tags/list', {
-        headers: { Authorization: `Bearer ${token}` }
-    }).then((r) => r.json());
+    const token = (
+        await fetch('https://ghcr.io/token?scope=repository:sillsdev/app-builders:pull').then((r) =>
+            r.json()
+        )
+    ).token as string;
 
-    const tags = tagJSON.tags as string[];
+    const tags = (
+        await fetch('https://ghcr.io/v2/sillsdev/app-builders/tags/list', {
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((r) => r.json())
+    ).tags as string[];
 
-    const latest = tags
-        .filter((t) => t.match(verNumRegExp))
-        .sort(compareVersions)
-        .at(-1);
+    const sortedTags = tags.filter((t) => t.match(verNumRegExp)).sort(compareVersions);
 
-    console.log(`appbuilder_version=${latest}`);
+    const latestDigest = await getDigest(token, 'latest');
+
+    let i = sortedTags.length;
+    let digest = '';
+    do {
+        i--;
+        digest = await getDigest(token, sortedTags[i]);
+    } while (i > 0 && latestDigest !== digest);
+
+    console.log(`appbuilder_version=${sortedTags[i]}`);
 })();
