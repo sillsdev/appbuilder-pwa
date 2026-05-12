62aa25af309a4fa053f2de90ff4270a70cb04be7
Aidan Jones <aejones4gm@gmail.com>
2026-05-12 14:50:47 -0500

    Use TS to fetch tags (#940)
    * Use TS to fetch tags
    
    * Compare manifests to get the right tag
    
    * Address coderabbit feedback

---
+++ convert/fetchTags.ts
@@ -0,0 +1,47 @@
+import { exit } from 'node:process';
+import { compareVersions } from './stringUtils';
+
+const verNumRegExp = /^\d+(\.\d+){0,2}$/;
+
+async function getDigest(token: string, ref: string) {
+    return (
+        await fetch(`https://ghcr.io/v2/sillsdev/app-builders/manifests/${ref}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        }).then((r) => r.json())
+    ).config.digest as string;
+}
+
+(async () => {
+    try {
+        const token = (
+            await fetch('https://ghcr.io/token?scope=repository:sillsdev/app-builders:pull').then(
+                (r) => r.json()
+            )
+        ).token as string;
+
+        const tags = (
+            await fetch('https://ghcr.io/v2/sillsdev/app-builders/tags/list', {
+                headers: { Authorization: `Bearer ${token}` }
+            }).then((r) => r.json())
+        ).tags as string[];
+
+        const sortedTags = tags.filter((t) => t.match(verNumRegExp)).sort(compareVersions);
+
+        const latestDigest = await getDigest(token, 'latest');
+
+        let i = sortedTags.length;
+        let digest = '';
+        do {
+            i--;
+            digest = await getDigest(token, sortedTags[i]);
+        } while (i > 0 && latestDigest !== digest);
+
+        console.log(`appbuilder_version=${sortedTags[i]}`);
+    } catch (e) {
+        console.error(e);
+        exit(1);
+    }
+})().catch((r) => {
+    console.error(r);
+    exit(1);
+});
