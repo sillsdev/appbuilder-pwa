f6a139720603d695dd8a909954febbf3e6344376
Aidan Jones <aejones4gm@gmail.com>
2026-05-12 14:44:32 -0500

    Convert styles to use `var(--color)` correctly (#954)
    * Convert styles to use `var(--color)` correctly
    
    * Fix `currentColor` false positive
    
    * Scope replacement correctly
    
    * Fix formatting
    
    * Make sure the style match ends at a semicolon

--- convert/convertStyles.ts
+++ convert/convertStyles.ts
@@ -101,6 +101,14 @@ export function convertStyles(dataDir: string, configData: ConfigTaskOutput, ver
                         }
                         line = line.replace('/fonts', joinUrlPath('$assets', finalPath));
                     }
+                    // fix CSS color var usage that doesn't use `var(--var)` syntax
+                    const colorMatch = line.match(/(color): ((?!var\(--|current)[^;]*Color[^;]*)/i);
+                    if (colorMatch) {
+                        line = line.replace(
+                            colorMatch[0],
+                            `${colorMatch[1]}: var(--${colorMatch[2]})`
+                        );
+                    }
                     return line;
                 })
                 .join(eol);
