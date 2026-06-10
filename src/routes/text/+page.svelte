78ba0738474cbe75f2c2705b1e1aa3b2b35bdf43
Awest882 <Awest882@students.bju.edu>
2026-06-10 15:49:11 -0500

    Swipe action fixed for storybooks

--- src/routes/text/+page.svelte
+++ src/routes/text/+page.svelte
@@ -707,7 +707,7 @@
                     <div
                         class="p-2 w-full"
                         style="position: absolute; left: {-draggableWidth}px; clip-path: inset(0 0 0 {draggableWidth -
-                            x.current}px"
+                            x.current}px);"
                     >
                         <main>
                             <div class="max-w-screen-md mx-auto">
@@ -770,6 +770,24 @@
                             </div>
                         </main>
                     </div>
+                </div>
+
+                <div
+                    class="p-2 w-full"
+                    style="position: absolute; left: {draggableWidth}px; clip-path: inset(0 {draggableWidth +
+                        x.current}px 0 0);"
+                >
+                    <main>
+                        <div class="max-w-screen-md mx-auto">
+                            {#if format === 'html'}
+                                <HtmlBookView {...nextSettings as HtmlBookViewProps} />
+                            {:else}
+                                <ScriptureViewSofria
+                                    {...nextSettings as ScriptureViewSofriaProps}
+                                />
+                            {/if}
+                        </div>
+                    </main>
                 </div>
                 <div class="hidden basis-1/12 md:flex justify-center">
                     <button
