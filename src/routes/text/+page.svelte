8233af3b64d5c550cedb43bd30f5c221d52d458d
Awest882 <Awest882@students.bju.edu>
2026-06-22 12:02:41 -0400

    Fixed border rendering

--- src/routes/text/+page.svelte
+++ src/routes/text/+page.svelte
@@ -808,8 +808,8 @@
         {/if}
         <!-- <div class="overflow-y-auto grow" bind:this={scrollingDiv} onscroll={saveScrollPosition}> -->
         <div
-            style="--borderImageSource: url({borders['./border.png']});"
-            class:borderimg={showBorder}
+            // style="--borderImageSource: url({borders['./border.png']});"
+            // class:borderimg={showBorder}
             class="overflow-y-auto grow overflow-x-hidden"
             bind:this={scrollingDiv}
             onscroll={saveScrollPosition}
@@ -872,6 +872,8 @@
                     >
                         <main>
                             <div
+                                style="--borderImageSource: url({borders['./border.png']});"
+                                class:borderimg={showBorder}
                                 aria-hidden="true"
                                 class="max-w-screen-md mx-auto"
                                 onpointerdown={handleMouseDown}
