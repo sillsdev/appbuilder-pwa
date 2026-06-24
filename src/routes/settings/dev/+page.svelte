e93f8d0abbbe62a2cdb2a9e0c682797d169712c2
Chris Hubbard <chris@thehubbards.org>
2026-06-24 17:23:27 -0400

    Show build info in /settings/dev route

--- src/routes/settings/dev/+page.svelte
+++ src/routes/settings/dev/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import { buildInfo } from '$lib/build-info';
     import Navbar from '$lib/components/Navbar.svelte';
     import Settings from '$lib/components/Settings.svelte';
     import { direction, t } from '$lib/data/stores';
     import { devPreferenceSettings } from '$lib/data/stores/setting';
+
+    const showBuildInfo = buildInfo.version || buildInfo.gitHash || buildInfo.buildDate;
 </script>
 
 <div
@@ -21,5 +24,32 @@
     </div>
     <div class="overflow-y-auto">
         <Settings settings={devPreferenceSettings}></Settings>
+        {#if showBuildInfo}
+            <div class="settings-category">Build Info</div>
+            {#if buildInfo.version}
+                <div class="dy-form-control settings-item w-full max-w-lg">
+                    <div class="dy-label py-0">
+                        <div class="settings-title">Version</div>
+                        <span>{buildInfo.version}</span>
+                    </div>
+                </div>
+            {/if}
+            {#if buildInfo.gitHash}
+                <div class="dy-form-control settings-item w-full max-w-lg">
+                    <div class="dy-label py-0">
+                        <div class="settings-title">Git Hash</div>
+                        <span>{buildInfo.gitHash}</span>
+                    </div>
+                </div>
+            {/if}
+            {#if buildInfo.buildDate}
+                <div class="dy-form-control settings-item w-full max-w-lg">
+                    <div class="dy-label py-0">
+                        <div class="settings-title">Build Date</div>
+                        <span>{buildInfo.buildDate}</span>
+                    </div>
+                </div>
+            {/if}
+        {/if}
     </div>
 </div>
