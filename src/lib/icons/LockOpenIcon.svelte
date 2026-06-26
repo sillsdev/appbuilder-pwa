e5fceef2f1de02fa8c4d81ae5ac2bc2e018a7f92
TheNonPirate <thenonpirate@gmail.com>
2026-06-26 10:55:41 -0600

    Begin work on quiz locked screen

---
+++ src/lib/icons/LockOpenIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    // Lock Open from https://fonts.google.com/icons
+    // Filled = 1
+    // Weight = 400
+    // Grade = 0
+    // Optical size = 24px
+    let { color = 'black' } = $props();
+</script>
+
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill={color}
+    ><path
+        d="M240-640h360v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85h-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640Zm296.5 336.5Q560-327 560-360t-23.5-56.5Q513-440 480-440t-56.5 23.5Q400-393 400-360t23.5 56.5Q447-280 480-280t56.5-23.5Z"
+    /></svg
+>
