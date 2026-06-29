0b549268fc73d7c627e7fe505633f28cdbd79dad
Chris Hubbard <chris@thehubbards.org>
2026-06-29 15:04:15 -0400

    Remove unnecessary styles and divs

--- src/lib/components/LayoutOptions.svelte
+++ src/lib/components/LayoutOptions.svelte
@@ -95,46 +95,39 @@ Displays the three different layout option menus.
             </p>
         {/if}
         {#each $selectedLayouts.sideBySide as collection, i}
-            <div>
-                <div class="max-w-screen-md mx-auto">
-                    <div class="px-3 layout-subtitle">
-                        {i + 1}
+            <div class="px-3 layout-subtitle">
+                {i + 1}
+            </div>
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div
+                class="flex justify-between layout-item-block rounded-none cursor-pointer"
+                onclick={() => {
+                    modal.open(ModalType.Collection, {
+                        type: 'double-pane',
+                        showBlank: false,
+                        number: i
+                    });
+                }}
+            >
+                {#if collection.image}
+                    <div class="layout-image-block self-start">
+                        <!-- svelte-ignore a11y_missing_attribute -->
+                        <img class="layout-image" src={illustrations['./' + collection.image]} />
                     </div>
-                    <!-- svelte-ignore a11y_click_events_have_key_events -->
-                    <!-- svelte-ignore a11y_no_static_element_interactions -->
-                    <div
-                        class="flex justify-between layout-item-block rounded-none cursor-pointer"
-                        onclick={() => {
-                            modal.open(ModalType.Collection, {
-                                type: 'double-pane',
-                                showBlank: false,
-                                number: i
-                            });
-                        }}
-                    >
-                        {#if collection.image}
-                            <div class="layout-image-block self-start">
-                                <!-- svelte-ignore a11y_missing_attribute -->
-                                <img
-                                    class="layout-image"
-                                    src={illustrations['./' + collection.image]}
-                                />
-                            </div>
-                        {/if}
-                        <div class="layout-text-block">
-                            <div class="layout-item-name">
-                                {collection.name}
-                            </div>
-                            {#if collection.description}
-                                <div class="layout-item-description">
-                                    {collection.description}
-                                </div>
-                            {/if}
-                        </div>
-                        <div class="px-3">
-                            <DropdownIcon color={$s?.['ui.layouts.selector'].color} />
-                        </div>
+                {/if}
+                <div class="layout-text-block">
+                    <div class="layout-item-name">
+                        {collection.name}
                     </div>
+                    {#if collection.description}
+                        <div class="layout-item-description">
+                            {collection.description}
+                        </div>
+                    {/if}
+                </div>
+                <div class="px-3">
+                    <DropdownIcon color={$s?.['ui.layouts.selector'].color} />
                 </div>
             </div>
         {/each}
@@ -146,46 +139,39 @@ Displays the three different layout option menus.
             </p>
         {/if}
         {#each $selectedLayouts.verseByVerse as collection, i}
-            <div>
-                <div class="max-w-screen-md mx-auto">
-                    <div class="px-3 layout-subtitle">
-                        {i + 1}
+            <div class="px-3 layout-subtitle">
+                {i + 1}
+            </div>
+            <!-- svelte-ignore a11y_click_events_have_key_events -->
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div
+                class="flex justify-between layout-item-block rounded-none cursor-pointer"
+                onclick={() => {
+                    modal.open(ModalType.Collection, {
+                        type: 'verse-by-verse',
+                        showBlank: i === 2,
+                        number: i
+                    });
+                }}
+            >
+                {#if collection.image}
+                    <div class="layout-image-block self-start">
+                        <!-- svelte-ignore a11y_missing_attribute -->
+                        <img class="layout-image" src={illustrations['./' + collection.image]} />
                     </div>
-                    <!-- svelte-ignore a11y_click_events_have_key_events -->
-                    <!-- svelte-ignore a11y_no_static_element_interactions -->
-                    <div
-                        class="flex justify-between layout-item-block rounded-none cursor-pointer"
-                        onclick={() => {
-                            modal.open(ModalType.Collection, {
-                                type: 'verse-by-verse',
-                                showBlank: i === 2,
-                                number: i
-                            });
-                        }}
-                    >
-                        {#if collection.image}
-                            <div class="layout-image-block self-start">
-                                <!-- svelte-ignore a11y_missing_attribute -->
-                                <img
-                                    class="layout-image"
-                                    src={illustrations['./' + collection.image]}
-                                />
-                            </div>
-                        {/if}
-                        <div class="layout-text-block">
-                            <div class="layout-item-name">
-                                {collection.name}
-                            </div>
-                            {#if collection.description}
-                                <div class="layout-item-description">
-                                    {collection.description}
-                                </div>
-                            {/if}
-                        </div>
-                        <div class="px-3">
-                            <DropdownIcon color={$s?.['ui.layouts.selector'].color} />
-                        </div>
+                {/if}
+                <div class="layout-text-block">
+                    <div class="layout-item-name">
+                        {collection.name}
                     </div>
+                    {#if collection.description}
+                        <div class="layout-item-description">
+                            {collection.description}
+                        </div>
+                    {/if}
+                </div>
+                <div class="px-3">
+                    <DropdownIcon color={$s?.['ui.layouts.selector'].color} />
                 </div>
             </div>
         {/each}
