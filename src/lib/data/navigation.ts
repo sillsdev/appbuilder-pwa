sillsdev/appbuilder-pwa

commit b7810b0fb98a84bc7ecca15066b38bb63ca17d49

fix: handle discontinuous chapter ranges

src/lib/data/navigation.ts
@@ -85,13 +85,15 @@ export class NavigationContext {
 
         let useFallbackChapter = false;
         const chapterNum = parseInt(chapter, 10);
-        const chapterRange = book?.chaptersN?.split('-').map((n) => parseInt(n, 10));
-        const fallbackChapter = String(chapterRange?.[0] || 1);
+        const chapterRange = book?.chaptersN
+            ?.split(' ')
+            .map((range) => range.split('-').map((n) => parseInt(n, 10)));
+        const fallbackChapter = String(chapterRange?.[0][0] || 1);
         if (
             (isNaN(chapterNum) && chapter !== 'i') ||
-            !chapterRange ||
-            chapterNum < chapterRange[0] ||
-            chapterNum > (chapterRange[1] || chapterRange[0])
+            !chapterRange?.some(
+                (range) => chapterNum >= range[0] && chapterNum <= (range[1] || range[0])
+            )
         ) {
             console.error(
                 `chapter '${chapter}' falls outside of chapter range '${book?.chaptersN}'. Defaulting to '${fallbackChapter}'.`

commit c558bf8cd54662bbce21abcb2fb0b9c81a1a9e40

fix: fix broken chapter fallback
Fix regressions introduced by https://github.com/sillsdev/appbuilder-pwa/commit/b7810b0fb98a84bc7ecca15066b38bb63ca17d49

src/lib/data/navigation.ts
@@ -90,7 +90,8 @@ export class NavigationContext {
             .map((range) => range.split('-').map((n) => parseInt(n, 10)));
         const fallbackChapter = String(chapterRange?.[0][0] || 1);
         if (
-            (isNaN(chapterNum) && chapter !== 'i') ||
+            isNaN(chapterNum) &&
+            chapter !== 'i' &&
             !chapterRange?.some(
                 (range) => chapterNum >= range[0] && chapterNum <= (range[1] || range[0])
             )