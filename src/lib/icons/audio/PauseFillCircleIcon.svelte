9d0fd6cf1f9eba26e4d0683f43cf1c901f776ce3
Aidan Jones <aidan_jones@sil.org>
2026-07-08 09:52:31 -0500

    fix: add missing filled-circle style
    How did we miss this, since it preexists the outline-circle style in SAB???

---
+++ src/lib/icons/audio/PauseFillCircleIcon.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    // Pause Circle from https://fonts.google.com/icons
+    // Filled = 1
+    // Weight = 400
+    // Grade = 0
+    // Optical size = 24px
+    let { color = 'black', size = 24 } = $props();
+</script>
+
+<svg
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+    height={size}
+    viewBox="0 -960 960 960"
+    width={size}
+    ><path
+        d="M360-320h80v-320h-80v320Zm160 0h80v-320h-80v320ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"
+    /></svg
+>
