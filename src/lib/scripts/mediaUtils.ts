ce31c8ce28e3d22edb283a2ce4d6f447a80fa92e
Aidan Jones <aidan_jones@sil.org>
2026-07-09 09:35:43 -0500

    fix: return caught error from getBibleBrainUrl

--- src/lib/scripts/mediaUtils.ts
+++ src/lib/scripts/mediaUtils.ts
@@ -9,24 +9,28 @@ export async function getBibleBrainUrl(
     }>,
     getDamId: (src: AudioSource) => string | undefined
 ): Promise<{ error: unknown; path?: never } | { error?: never; path: string }> {
-    const dbp4 = audioSource.address ? audioSource.address : 'https://4.dbt.io';
-    // if (source.accessMethods.includes('download')) {
-    //    // TODO: Figure out how to use Cache API to download audio to PWA
-    // }
-    const damId = getDamId(audioSource);
-    const request = `${dbp4}/api/bibles/filesets/${damId}/${item.book}/${item.chapter}?v=4&key=${audioSource.key}`;
-    const response = await fetch(request, {
-        method: 'GET',
-        headers: {
-            accept: 'application/json'
+    try {
+        const dbp4 = audioSource.address ? audioSource.address : 'https://4.dbt.io';
+        // if (source.accessMethods.includes('download')) {
+        //    // TODO: Figure out how to use Cache API to download audio to PWA
+        // }
+        const damId = getDamId(audioSource);
+        const request = `${dbp4}/api/bibles/filesets/${damId}/${item.book}/${item.chapter}?v=4&key=${audioSource.key}`;
+        const response = await fetch(request, {
+            method: 'GET',
+            headers: {
+                accept: 'application/json'
+            }
+        });
+
+        const json = await response.json();
+
+        if (!json.error) {
+            return { path: json.data[0].path };
         }
-    });
 
-    const json = await response.json();
-
-    if (!json.error) {
-        return { path: json.data[0].path };
+        return json;
+    } catch (error) {
+        return { error };
     }
-
-    return json;
 }
