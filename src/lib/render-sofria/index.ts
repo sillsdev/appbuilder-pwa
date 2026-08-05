8b7a8d09c4e4ae3a2b5df1f68e16a8b73539323f
benpaj-cps <benjamin.pajunen@gordon.edu>
2026-08-04 20:58:23 -0400

    Update docstring, move main text feature into separate file

--- src/lib/render-sofria/index.ts
+++ src/lib/render-sofria/index.ts
@@ -1,4 +1,5 @@
 import type { FeatureSpec } from './common';
 import documentFeature from './DocumentFeature';
+import mainTextFeature from './MainTextFeature';
 
-export const renderFeatures: Array<FeatureSpec> = [documentFeature];
+export const renderFeatures: Array<FeatureSpec> = [documentFeature, mainTextFeature];
